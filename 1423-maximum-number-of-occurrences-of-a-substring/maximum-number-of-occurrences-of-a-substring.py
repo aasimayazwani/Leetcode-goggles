47b3ee3f7a2cfb8dd74b75d25278b523fd01d841
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-06 00:52:33 -0800

    Time: 4449 ms (5.05%) | Memory: 127.2 MB (15.90%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.py
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.py
@@ -1,24 +1,29 @@
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
         pairs = {}
-        for size in range(minSize, maxSize + 1):
-            current = s[:size-1]
-            for i in range(size-1, len(s)):
+        for size in range(minSize,maxSize+1):
+            current = s[0:size-1]
+            for i in range(size-1,len(s)):
                 current = current + s[i]
-                if self.count(current, maxLetters):
+                if self.count(current,maxLetters) == True:
                     if current in pairs:
-                        pairs[current] += 1
-                    else:
-                        pairs[current] = 1
+                        pairs[current] +=1 
+                    if current not in pairs:
+                        pairs[current] = 1 
                     current = current[1:]
                 else:
                     current = current[1:]
-
-        # Check if pairs is empty before finding the max
+                    pass
+        if pairs:   
+            return max([item[1] for item in pairs.items()])
         if not pairs:
-            return 0
+            return 0 
 
-        return max([item[1] for item in pairs.items()])
 
     def count(self, string, maxLetters):
-        return len(set(string)) <= maxLetters
+        size = set()
+        for i in range(len(string)):
+            size.add(string[i])
+            if len(size) > maxLetters:
+                return False
+        return True
