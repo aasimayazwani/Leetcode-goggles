76eabfd8478880649974d34cadf6254e295d6931
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-11 22:02:40 -0800

    Time: 3081 ms (5.81%) | Memory: 30.1 MB (9.30%) - LeetSync

--- 2890-count-substrings-without-repeating-character/count-substrings-without-repeating-character.py
+++ 2890-count-substrings-without-repeating-character/count-substrings-without-repeating-character.py
@@ -1,7 +1,6 @@
 class Solution:
     def numberOfSpecialSubstrings(self, s: str) -> int:
         mapping = self.generate(s)
-
         count = 0
         for left in range(0,len(s)):
             cur = set()
@@ -12,15 +11,7 @@ class Solution:
                 else:
                     break
         return count
-    
-    def check_unique(self,string):
-        current = set()
-        for i in range(0,len(string)):
-            if string[i] in current:
-                return False
-            if string[i] not in current:
-                current.add(string[i]) 
-        return True
+
 
     def generate(self,string):
         mapping = {}
