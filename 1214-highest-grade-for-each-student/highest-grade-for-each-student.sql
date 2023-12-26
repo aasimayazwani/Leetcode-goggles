81fcac36c263c3faabf2d9b071ba0a78fc7adda0
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-25 22:40:10 -0800

    Time: 1004 ms (32.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select student_id, course_id, grade from 
+(select *, dense_rank() over (partition by student_id order by grade desc, course_id asc) as "ranking"
+from enrollments ) as t1 
+where ranking = 1 
+order by student_id asc ; 
