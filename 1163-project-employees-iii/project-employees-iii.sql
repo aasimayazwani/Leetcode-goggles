1a46a08826ffced01687cc0973106aaf7e342433
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-26 15:07:27 -0800

    Time: 921 ms (97.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with cte as (select t1.project_id,t1.employee_id, t2.experience_years  
+from project as t1
+inner join employee as t2 
+on t1.employee_id = t2.employee_id) 
+
+select project_id, employee_id from 
+(select *, dense_rank() over (partition by project_id order by experience_years desc) as "ranking"
+from cte ) as t4 
+where ranking = 1 ;
