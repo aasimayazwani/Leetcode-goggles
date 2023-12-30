2e4a4fc3bdcdfa2884c22b6a38489f55402eb484
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 13:28:17 -0800

    Time: 1835 ms (48.30%) | Memory: 0B (100.00%) - LeetSync

--- 1172-sales-analysis-i/sales-analysis-i.sql
+++ 1172-sales-analysis-i/sales-analysis-i.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-with cte as (select seller_id, sum(price) as "total" from sales
+with cte as
+(select seller_id, sum(price) as "total"
+from sales
 group by seller_id )
 
-select seller_id from 
-(select *, dense_rank() over (order by total desc) as "ranking"
-from cte)  as t3 
-where ranking = 1 ;
+
+select seller_id from (select *, dense_rank() over (order by total desc) as "ranking"
+from cte ) as t4 
+where ranking = 1 ; 
