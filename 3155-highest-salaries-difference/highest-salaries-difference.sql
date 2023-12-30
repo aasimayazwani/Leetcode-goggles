1310a24ef75452fe46f200bcd982361c009bd5f7
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 13:18:24 -0800

    Time: 594 ms (49.09%) | Memory: 0B (100.00%) - LeetSync

--- 3155-highest-salaries-difference/highest-salaries-difference.sql
+++ 3155-highest-salaries-difference/highest-salaries-difference.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-select abs(max(t1.salary) - max(t2.salary)) as "salary_difference" from salaries as t1 
-inner join salaries as t2
-on t1.department = "Engineering" and 
-t2.department = "Marketing" ; 
+with cte as 
+(select * from
+(select *, dense_rank() over (partition by department order by salary desc) as "ranking"
+from salaries) as t1 
+where ranking = 1 )
+
+select abs(t3.salary-t2.salary) as "salary_difference" from cte as t2 , 
+cte as t3 
+where t2.department = "Engineering" and 
+t3.department = "Marketing"
