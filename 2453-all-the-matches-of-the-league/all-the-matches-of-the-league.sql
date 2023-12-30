6364f06d3ebd6945799dcc95540af7e3dc96b515
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 11:26:10 -0800

    Time: 947 ms (58.31%) | Memory: 0B (100.00%) - LeetSync

--- 2453-all-the-matches-of-the-league/all-the-matches-of-the-league.sql
+++ 2453-all-the-matches-of-the-league/all-the-matches-of-the-league.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
 select t1.team_name as "home_team",
-    t2.team_name as "away_team"
- from teams as t1
-inner join 
-teams as t2 
+t2.team_name as "away_team"
+from teams as t1, teams as t2 
 where t1.team_name != t2.team_name ; 
