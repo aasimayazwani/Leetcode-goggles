f6efe008eea3640602a02e5bcc7150027a14d87d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 13:49:23 -0800

    Time: 581 ms (88.74%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement belo
-select actor_id, director_id from 
-(select *, count(*)  as "counting"
-from ActorDirector
-group by actor_id, director_id ) as t1
-where counting >= 3 ; 
+select distinct actor_id, director_id from 
+(select actor_id, director_id, count(timestamp) as "total"
+from actordirector
+group by actor_id, director_id 
+having total >= 3 ) as t4 ; 
