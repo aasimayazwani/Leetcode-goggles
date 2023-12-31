b9203ed27b1961cd8d3a605c3252e65cef26febc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-31 00:25:13 -0800

    Time: 1461 ms (76.97%) | Memory: 0B (100.00%) - LeetSync

--- 2452-product-sales-analysis-v/product-sales-analysis-v.sql
+++ 2452-product-sales-analysis-v/product-sales-analysis-v.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
-select t1.user_id, sum(t1.quantity*t2.price) as "spending" from sales as t1 
+select t1.user_id, ifnull(sum(t1.quantity*t2.price),0) as "spending"  from sales as t1
 inner join 
 product as t2 
-on t1.product_id = t2.product_id
+on t1.product_id = t2.product_id 
 group by t1.user_id 
-order by spending desc, user_id asc  ; 
+order by spending desc, user_id asc ;
