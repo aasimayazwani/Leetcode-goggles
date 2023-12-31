cc596fe4192fc332a2bd250506ff21330d442a21
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 19:30:10 -0800

    Time: 1671 ms (63.69%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,6 +1,6 @@
 select seller_name from seller 
 where seller_id not in (
-    select seller_id from orders 
-    where year(sale_date) = 2020
+    select distinct seller_id from orders
+    where year(sale_date) = 2020 
 )
 order by seller_name asc ; 
