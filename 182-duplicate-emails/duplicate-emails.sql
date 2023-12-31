7c202a9081f3a21e0945cdbe9cf4d7f51337c871
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 22:15:45 -0800

    Time: 958 ms (13.03%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-select distinct email  as "Email" from
-(select *, count(email) over (partition by email)
-as "counting"
+select distinct email from 
+(select email, count(email) over (partition by email) as "counting"
 from person ) as t1 
-where counting > 1  ; 
+where counting > 1 ; 
