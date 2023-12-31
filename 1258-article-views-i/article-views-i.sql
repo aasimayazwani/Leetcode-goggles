533dc291a568337d5af9d21e123a49416af8862f
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 22:21:14 -0800

    Time: 680 ms (79.82%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,4 +1,4 @@
-select distinct author_id as "id"
-from views where 
-author_id = viewer_id 
-order by id asc ; 
+# Write your MySQL query statement below
+select distinct author_id as "id" from views 
+where author_id = viewer_id 
+order by author_id asc ; 
