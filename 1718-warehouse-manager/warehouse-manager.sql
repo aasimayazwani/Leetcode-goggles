9cf3554255b49588aaa7b206eb43b17f3228d612
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 23:24:18 -0800

    Time: 1052 ms (79.29%) | Memory: 0B (100.00%) - LeetSync

--- 1718-warehouse-manager/warehouse-manager.sql
+++ 1718-warehouse-manager/warehouse-manager.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
-with cte as 
-(select product_id, 
-Width*Length*Height as "current"
-from Products) 
-
-select t2.name as "warehouse_name",
-sum(t2.units*t1.current) as "volume"
- from cte as t1 inner join 
-warehouse as t2
-on t1.product_id = t2.product_id 
-group by t2.name ; 
+select t1.name as "warehouse_name", sum(t1.units*t2.width*t2.height*t2.length) as "volume" 
+from Warehouse as t1 
+inner join 
+products as t2 
+on 
+t1.product_id = t2.product_id
+group by t1.name ; 
