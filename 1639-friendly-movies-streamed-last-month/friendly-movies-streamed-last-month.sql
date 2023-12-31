ef91b9e7d3a02b666fbe3b276aff436e13ac17e8
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 19:51:51 -0800

    Time: 1652 ms (68.11%) | Memory: 0B (100.00%) - LeetSync

--- 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
+++ 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
@@ -1,5 +1,8 @@
-select distinct t2.title from tvprogram as t1 
-inner join content as t2 
+select distinct title from 
+(select t2.title,t1.program_date from tvprogram as t1 
+left join 
+content as t2 
 on t1.content_id = t2.content_id 
-and Kids_content = "Y" and content_type = "Movies" and 
-date_format(program_date,"%Y-%m") = "2020-06" ; 
+where t2.kids_content = "Y" and 
+t2.content_type = "Movies"
+having date_format(t1.program_date,"%Y-%m") = "2020-06")as t3  ; 
