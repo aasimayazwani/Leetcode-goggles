cfceeb451b01fbf95876ba86332f46af4e1028eb
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-30 19:02:16 -0800

    Time: 806 ms (40.21%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-select ifnull(max(num),null) as "num" from
-(select *, count(num) as "counting"
-from mynumbers 
-group by num ) as t1 
-where counting = 1 
+select ifnull(max(num),null) as "num" from 
+(select *, count(num) over (partition by num) as "counting"
+from mynumbers ) as t1 
+where counting = 1 ; 
