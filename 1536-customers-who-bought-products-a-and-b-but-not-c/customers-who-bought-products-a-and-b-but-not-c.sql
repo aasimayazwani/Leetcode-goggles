ebf1a35048e9fc5ab944a707b54915477ef6845e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-01 08:39:53 -0800

    Time: 1079 ms (88.87%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,15 +1,12 @@
 # Write your MySQL query statement below
 with cte as 
-(select t1.customer_id from orders as t1 
-inner join orders as t2 
-on t1.customer_id = t2.customer_id 
-where t1.product_name ="A" and 
-t2.product_name = "B" 
-and t1.customer_id not in (select customer_id from orders 
-where product_name = "C") )
+(select customer_id from 
+(select distinct t1.customer_id from Orders as t1 , Orders as t2 
+where t1.customer_id  = t2.customer_id and 
+t1.product_name = "A" and t2.product_name = "B") as t3 
+where customer_id not in (select distinct customer_id from orders where 
+product_name ="C"))
 
-
-select distinct t3.customer_id, t3.customer_name from customers as t3
-inner join cte as t4 
-on t4.customer_id = t3.customer_id 
-order by t3.customer_id asc ; 
+select cte.customer_id, customers.customer_name from cte inner join customers 
+on customers.customer_id = cte.customer_id
+order by customer_id asc ;  
