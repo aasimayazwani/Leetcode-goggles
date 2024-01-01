5fe5ceee091e415aa880129b6a0edaec94701efc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2023-12-31 20:00:33 -0800

    Time: 1576 ms (80.62%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
-select product_id, year as "first_year", quantity, price from 
-(select *, dense_rank() over (partition by product_id order by year asc) 
+with cte as
+(select * from 
+(select *, dense_rank() over (partition by product_id order by year asc )
 as "ranking"
-from sales ) as t1 
-where ranking = 1 ;
+ from sales) as t1 
+ where ranking = 1 )
+
+ select cte.product_id, cte.year as "first_year",
+ cte.quantity, cte.price
+  from cte 
+ inner join product as t2 
+ on cte.product_id = t2.product_id ; 
