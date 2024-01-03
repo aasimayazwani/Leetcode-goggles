72bffcbd6e16b23de15500e40eaa7f9bae8dfbfd
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-03 00:11:07 -0800

    Time: 660 ms (34.39%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,5 @@
-# Write your MySQL query statement below
-with cte as 
-(select managerid, count(id) as "counting" 
-from employee 
-group by managerid 
-having counting >= 5) 
-
-select t2.name from cte as t1 
-inner join employee as t2 
-on t1.managerid = t2.id ; 
+select e1.name from employee e1 inner join (
+select id, name, count(*) as total, managerId from employee 
+group by managerId 
+having total >= 5) as t 
+on e1.id = t.managerid 
