e34f175ecfcfe51200020fa8901f031e55802bf1
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-02 21:19:11 -0800

    Time: 2303 ms (23.42%) | Memory: 0B (100.00%) - LeetSync

--- 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
+++ 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
@@ -1,11 +1,14 @@
 # Write your MySQL query statement below
-select candidate_id from
-(select 
-t1.candidate_id,
-avg(t1.years_of_exp) as "experience",
-sum(t2.score) as "total_score"
-from candidates as t1 
-inner join rounds as t2 
-on t1.interview_id = t2.interview_id
-group by t1.candidate_id 
-having total_score > 15 and experience >= 2) as t4   ; 
+with cte as 
+(select t1.candidate_id,t1.years_of_exp,t1.interview_id,
+        t2.round_id,t2.score
+ from candidates as t1 
+inner join 
+rounds as t2 
+on t1.interview_id = t2.interview_id) 
+
+select distinct candidate_id from 
+(select *, sum(score) over (partition by candidate_id) as "total"
+from cte ) as t3 
+where total > 15 and 
+years_of_exp >= 2 ; 
