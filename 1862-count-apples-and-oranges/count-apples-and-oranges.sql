4feff6ac8c4f5d35ccd437c97e10fc342ac53aec
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-05 15:35:49 -0800

    Time: 1568 ms (93.85%) | Memory: 0B (100.00%) - LeetSync

--- 1862-count-apples-and-oranges/count-apples-and-oranges.sql
+++ 1862-count-apples-and-oranges/count-apples-and-oranges.sql
@@ -1,9 +1,9 @@
-SELECT 
-    SUM(t1.apple_count + IFNULL(t2.apple_count, 0)) AS apple_count,
-    SUM(t1.orange_count + IFNULL(t2.orange_count, 0)) AS orange_count
-FROM 
-    boxes AS t1 
-LEFT JOIN 
-    chests AS t2 
-ON 
-    t1.chest_id = t2.chest_id;
+with cte as (select t1.apple_count + ifnull(t2.apple_count,0) as "apple_count",
+        t1.orange_count + ifnull(t2.orange_count,0) as "orange_count"
+ from boxes as t1 
+left join chests as t2 
+on t1.chest_id = t2.chest_id )
+
+select sum(apple_count) as "apple_count",
+        sum(orange_count) as "orange_count"
+        from cte ; 
