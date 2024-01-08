60687a9209ac8e73f1c1c5340be9015ca25dc80f
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-07 19:42:39 -0800

    Time: 806 ms (40.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select t1.firstname, t1.lastname, ifnull(t2.city,null ) as "city", ifnull(t2.state,null ) as "state" from person as t1 
+left join address as t2 
+on t1.personid = t2.personid ; 
