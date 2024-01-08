49b60d1a2acb6288aa3df76dc206b4014ae0a3c6
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-07 23:29:07 -0800

    Time: 1283 ms (32.01%) | Memory: 0B (100.00%) - LeetSync

--- 2840-immediate-food-delivery-iii/immediate-food-delivery-iii.sql
+++ 2840-immediate-food-delivery-iii/immediate-food-delivery-iii.sql
@@ -1,11 +1,14 @@
 # Write your MySQL query statement below
-with cte as
-(select order_date,customer_pref_delivery_date, count(delivery_id ) over (partition by order_date) as "total", 
-case when order_date =  customer_pref_delivery_date then 1 
-else 0 
-end as "reward"
-from delivery) 
+with cte as (select *, 
+case 
+    when order_date = customer_pref_delivery_date then 1 
+    else 0 
+    end 
+    as "counting", 
+    count(delivery_id) over (partition by order_date) as "total"
+from delivery)
 
-select order_date, round(((sum(reward)*100)/total),2) as "immediate_percentage" from cte 
+
+select order_date, round(sum(counting)*100/avg(total),2) as "immediate_percentage"
+from cte 
 group by order_date
-order by order_date asc ; 
