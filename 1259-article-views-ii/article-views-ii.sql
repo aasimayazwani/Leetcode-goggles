44afd1160452e99ca38eca6f19b458a548fe78a3
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-12 09:20:47 -0800

    Time: 1305 ms (7.81%) | Memory: 0B (100.00%) - LeetSync

--- 1259-article-views-ii/article-views-ii.sql
+++ 1259-article-views-ii/article-views-ii.sql
@@ -1,5 +1,7 @@
-select distinct viewer_id  as "id" from
-(select viewer_id, count(distinct article_id) as "unique_views" from views 
-group by view_date, viewer_id
-having unique_views > 1) as t1 
+select distinct viewer_id as "id" 
+from 
+(select viewer_id, view_date, count(distinct Article_id) as "counting"
+from views
+group by viewer_id, view_date) as t1 
+where counting > 1 
 order by viewer_id asc ; 
