e7825bed38139946877cc171d283f97c1ad6febb
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-11 16:14:37 -0800

    Time: 2089 ms (74.16%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,10 +1,14 @@
-with cte as (select buyer_id, count(order_id) as "orders_in_2019" from 
-orders 
-where  year(order_date) = 2019
-group by buyer_id) 
+with cte as
+(select 
+buyer_id as "user_id",
+count(order_id) as "orders_in_2019"
+from orders 
+where year(order_date) = 2019
+group by user_id ) 
 
-select t1.user_id as "buyer_id", t1.join_date, 
-ifnull(cte.orders_in_2019,0)  as "orders_in_2019" from users as t1 
-left join 
-cte on 
-t1.user_id = cte.buyer_id ; 
+select t1.user_id as "buyer_id", 
+t1.join_date ,
+ifnull(t2.orders_in_2019,0) as "orders_in_2019" 
+from users as t1 
+left join cte as t2 
+on t1.user_id = t2.user_id ; 
