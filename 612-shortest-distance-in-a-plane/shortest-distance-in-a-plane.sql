94b4083f3aacdf5e3edf0ff55d831df42638418c
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-12 18:21:28 -0800

    Time: 490 ms (31.16%) | Memory: 0B (100.00%) - LeetSync

--- 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
+++ 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
 with cte as
-(select sqrt((t1.x - t2.x)*(t1.x - t2.x) + 
-            (t1.y-t2.y)*(t1.y-t2.y)) as "shortest" 
-from point2d as t1, point2d as t2 
-having shortest != 0)
+(select *, row_number() over () as "numbering"
+from point2d)
 
-select round(min(shortest),2) as "shortest" from cte ; 
+select round(min(shortest),2) as "shortest" from 
+(select sqrt((t1.x - t2.x)*(t1.x - t2.x) +
+        (t1.y - t2.y)*(t1.y - t2.y)) as "shortest"
+ from cte as t1, cte as t2 
+where t1.numbering != t2.numbering) as t4 
