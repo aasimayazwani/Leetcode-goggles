c514e5fb336ef357dba6a8b7e8bd1898c50098b1
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-16 09:21:32 -0800

    Time: 105 ms (85.59%) | Memory: 17.8 MB (33.27%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -4,5 +4,5 @@ class Solution:
         for i in range(0,len(names)):
             mapping[heights[i]] = names[i]
         
-        sorted_arr = sorted(mapping.items(), key = lambda x:x[0])[::-1]
+        sorted_arr = sorted(mapping.items(), key = lambda x:x[0],reverse = True )
         return [item[1] for item in sorted_arr]
