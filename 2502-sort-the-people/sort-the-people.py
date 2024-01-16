3a32b597933c3b024b2e82be633cc534a4bfc3f8
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-01-16 09:19:49 -0800

    Time: 102 ms (90.65%) | Memory: 17.9 MB (33.27%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        mapping = {}
+        for i in range(0,len(names)):
+            mapping[heights[i]] = names[i]
+        
+        sorted_arr = sorted(mapping.items(), key = lambda x:x[0])[::-1]
+        return [item[1] for item in sorted_arr]
