744f593b83ad1271c37a79a87acc89193cfcb778
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-02-01 13:56:37 -0800

    Time: 189 ms (10.83%) | Memory: 17.4 MB (47.06%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -3,38 +3,38 @@ class Solution:
         if numRows == 1:
             return s
 
-        matrix = []
-        counter = 0
-        while s:
-            #print("string is ",s)
-            if counter % (numRows-1) == 0:
-                fail_status = 1
-                current_array = [item for item in s[0:numRows]]
-                s  = s[numRows:] 
-                matrix.append(current_array)
-                counter += 1 
+        stack = []
+        i = 0
+        while len(s) > 0:
+            if i%2 == 0:
+                stack.append(s[0:numRows])
+                s = s[numRows:]
             else:
-                current_array  = [s[0]]
-                current_array = ["#"]*(numRows-1-fail_status) + current_array + ["#"]*fail_status
-                s =  s[1:]
-                counter += 1 
-                matrix.append(current_array)
-                fail_status += 1 
-        #print(matrix)
-        shifted = []
-        for row in range(0,numRows):
-            result = self.get_index(matrix,row)
-            result = [item for item in result if item != "#"]
-            if result != []:
-                shifted.append("".join(result))
-        return "".join(shifted)
+                stack.append(s[0:numRows-2])
+                s = s[numRows-2:]
+            i +=1
+            
+        second = []
+        for i in range(0,len(stack)):
+            if i %2 != 0:
+                second.extend(self.get_words(stack[i],numRows))
+            else:
+                second.append(stack[i])
+                
+                
+        for i in range(0,len(second)):
+            if len(second[i]) < numRows:
+                second[i] = second[i] + "*"*(numRows - len(second[i]))
+                
+        answer = []
+        for i in range(0,numRows):
+            answer.append("".join([item[i] for item in second if item[i] != "*"]))
+            
+        return "".join(answer)
 
-
-    def get_index(self,matrix,current_index):
-        current_array = []
-        for i in range(0,len(matrix)):
-            try:
-                current_array.append(matrix[i][current_index])
-            except :
-                pass
-        return current_array
+    def get_words(self,words,numRows):
+        collection = []
+        for i in range(0,len(words)):
+            new_word = "*"*(i+1) + words[i] + "*"*(numRows-i-2)
+            collection.append(new_word[::-1])
+        return collection
