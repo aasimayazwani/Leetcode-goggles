f7e08e2192b9298509386595c80a402afb15f227
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-02-03 12:12:12 -0800

    Time: 28 ms (92.99%) | Memory: 16.5 MB (76.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         mapping = {1:1,2:2}
-        
         def climb(n):
             if n in mapping:
                 return mapping[n]
             else:
                 mapping[n] = climb(n-1) + climb(n-2)
                 return mapping[n]
-        
         return climb(n)
