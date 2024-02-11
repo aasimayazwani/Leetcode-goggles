0237f6ac15da79a9f008eb390da4d273595b4e07
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-02-11 01:41:26 -0800

    Time: 89 ms (5.21%) | Memory: 17.4 MB (75.64%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,31 +6,28 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if root == None:
-            return False
-        else:
-            result = self.sumNumbers(root)
-            for i in range(0,len(result)):
-                if sum([eval(item) for item in result[i]]) == targetSum:
-                    return True
-            return False
-
-    def sumNumbers(self, root):
-        current  = [[root,str(root.val)]]
         values = []
-        answer = []
-        while current:
-            collections = []
-            for i in range(0,len(current)):
-                node = current[i][0]
-                if node.left:
-                    collections.append([node.left,current[i][1] + ">" +str(node.left.val)])
-                if node.right:
-                    collections.append([node.right,current[i][1] + ">"+str(node.right.val)])
-                if (node.left == None) and (node.right == None):
-                    answer.append(current[i][1])
-            current = collections
-            if len(current) > 0:
-                values.append(current)
-        answer = ([item.split(">") for item in answer])
-        return answer
+        if root == None:
+            return False 
+        root = [root,str(root.val)]
+        def dfs(root):
+            if root == None:
+                return
+            else:
+                child, path = root
+                if child.left != None:
+                    dfs([child.left,path + ">" + str(child.left.val)])
+
+                if child.left == None and child.right == None:
+                    path = ("".join(path.replace(">","+")))
+                    total = eval(path)
+                    values.append(total)
+
+                if child.right != None:
+                    dfs([child.right,path + ">" + str(child.right.val)])
+                
+                return values
+        current = dfs(root)
+        if targetSum in current:
+            return True 
+        return False
