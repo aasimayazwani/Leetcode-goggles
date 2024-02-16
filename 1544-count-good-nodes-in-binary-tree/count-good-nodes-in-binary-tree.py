9cd8d02a0cdac0361546739866750e72401d20b2
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-02-15 22:29:19 -0800

    Time: 132 ms (65.83%) | Memory: 31.5 MB (64.08%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -1,39 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        current = [[root,str(root.val) + ">"]] 
-        counting_good_nodes = 1
-        while current :
-            status = []
-            for i in range(0,len(current)):
-                
-                node = current[i]
-                node, story = node[0], node[1] 
-                
-                if node.left != None:
-                    updated_string = story + str(node.left.val) + ">" 
-                    status.append([node.left, updated_string ])
-                    current_status = self.check(updated_string)
-                    if current_status == True:
-                        counting_good_nodes += 1 
-                        
-                if node.right != None:
-                    updated_string = story + str(node.right.val) + ">"
-                    status.append([node.right, updated_string  ])
-                    current_status = self.check(updated_string)
-                    if current_status == True:
-                        counting_good_nodes += 1 
+        counting = []
+        def dfs(root,maximum):
+            if root == None:
+                return 
 
-                
-            current = status
-        return counting_good_nodes 
+            if root.val >= maximum:
+                #print(root.val)
+                maximum = root.val
+                counting.append(1)
+            dfs(root.left,maximum)
+            dfs(root.right,maximum)
 
-    def check(self,string):
-        split_string = [int(item) for item in string.split(">")[0:-1]]
-        #print([max(split_string), split_string[-1]])
-        return max(split_string)  == split_string[-1]
+        dfs(root,-900000)
+
+        return len(counting)
