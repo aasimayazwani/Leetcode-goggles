92e912616e969e2bea50b68d41e41fdb98f14d7e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-02-20 13:18:42 -0800

    Time: 838 ms (7.67%) | Memory: 224 MB (5.02%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,13 +1,22 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         counting = 0
+        success = {}
+        failed = {}
         for length in range(0,len(s)):
             current = s[0:length]
             for index in range(length,len(s)):
                 current += s[index]
-                #print(current)
-                if current == current[::-1]:
-                    counting +=1
+                if current in success:
+                    counting +=1 
+                elif current in failed:
+                    pass
+                else:
+                    if current == current[::-1]:
+                        counting +=1
+                        success[current] = 1
+                    else:
+                        failed[current] = 1 
                 current = current[1:]
 
         return counting
