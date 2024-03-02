f1b160a49bdf3903da268f3470735653cf2f313e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-02 12:17:34 -0800

    Time: 1430 ms (66.67%) | Memory: 0B (100.00%) - LeetSync

--- 2451-product-sales-analysis-iv/product-sales-analysis-iv.sql
+++ 2451-product-sales-analysis-iv/product-sales-analysis-iv.sql
@@ -1,12 +1,15 @@
-with cte as 
-(select t1.user_id, t1.product_id, sum(t1.quantity*t2.price) as "total" from sales as t1 
+with cte as
+(select t1.user_id, t1.product_id, sum(t1.quantity*t2.price) as "total" 
+from sales as t1 
 inner join product as t2 
-on t1.product_id = t2.product_id 
+on 
+t1.product_id = t2.product_id 
 group by t1.user_id, t1.product_id)
 
-
-select user_id, product_id from 
-(select *, dense_rank() over (partition by user_id order by total desc) as "ranking"
-from cte ) as t3 
-where 
-ranking = 1 ; 
+select user_id, product_id 
+from
+(select *,  
+dense_rank() over (partition by user_id order by total desc) as "ranking" 
+from 
+cte) as t5
+where ranking = 1 ;
