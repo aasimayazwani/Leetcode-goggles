173379c2422f2c0ea4945d4f9ff76731df917a72
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-02 18:19:08 -0800

    Time: 1273 ms (90.77%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,5 @@
-select person_name  from
-(select person_name, Total_Weight  from 
-(select 
-*, 
-sum(weight) over (order by turn asc) as "Total_Weight"
-from queue) as t1 
-where Total_Weight <= 1000 order by Total_Weight desc )  as t2 
-limit 1 ;
+select person_name from 
+(select *, sum(weight) over (order by turn) as "Total_Weight"
+from queue ) as t1  
+where Total_Weight <= 1000
+order by Total_Weight desc limit 1 ;
