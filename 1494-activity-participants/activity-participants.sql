a625846724a8f171a4dca8f5ff77be2230ca1712
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-02 21:33:27 -0800

    Time: 1659 ms (19.66%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,17 +1,13 @@
-with cte as 
-(select *, dense_rank() over (order by counting asc ) as "lowest",
-dense_rank() over (order by counting DESC ) as "highest"
-from 
-(select activity, count(activity) as "counting"
-from friends 
-group by activity
-) as t1 )
+# Write your MySQL query statement below
+with cte as
+(select activity,count(name) as "total" 
+from friends
+group by activity)
 
 
-select distinct  activity from cte 
-where activity not in 
-(select distinct activity from cte 
-where lowest = 1
-UNION ALL 
 select distinct activity from cte 
-where highest = 1 )
+where total not in (
+    select min(total) from  cte 
+    UNION ALL
+    select max(total) from  cte 
+)
