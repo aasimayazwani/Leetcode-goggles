c66cee57c6443e5d11fec74d9bc0f6533d26a0f2
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-03 08:03:03 -0800

    Time: 1250 ms (91.91%) | Memory: 0B (100.00%) - LeetSync

--- 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
+++ 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
-with cte as
-(select t1.team_id,t1.name, t1.points, t1.points + t2.points_change as "new_points" from  teampoints as t1 
-inner join pointschange as t2 
-on t1.team_id = t2.team_id)
+with cte as 
+(select *, dense_rank() over (order by points1 desc, name asc ) as "ranking1",
+          dense_rank() over (order by points2 desc, name asc) as "ranking2"
+from 
+(select t1.team_id, t1.name, 
+t1.points as "points1", 
+t1.points + t2.points_change as "points2"
+from teampoints as t1 
+inner join 
+pointschange as t2 
+on t1.team_id = t2.team_id) as t1 )
 
-select distinct team_id, name, cast(ranking as signed) - cast(new_ranking as signed)  as "rank_diff" from 
-(select *, dense_rank() over (order by points desc, name asc) as "ranking", 
-          dense_rank() over (order by new_points desc, name asc) as "new_ranking"
-          from cte) as t4 ; 
+select team_id, name, cast(ranking1 as signed) - cast(ranking2 as signed) as "rank_diff" from 
+cte ; 
