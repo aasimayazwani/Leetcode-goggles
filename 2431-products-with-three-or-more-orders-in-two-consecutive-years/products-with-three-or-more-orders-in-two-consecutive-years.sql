e0a2b2519bf8b515235985473f1380fc32889496
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-02 19:57:02 -0800

    Time: 794 ms (92.96%) | Memory: 0B (100.00%) - LeetSync

--- 2431-products-with-three-or-more-orders-in-two-consecutive-years/products-with-three-or-more-orders-in-two-consecutive-years.sql
+++ 2431-products-with-three-or-more-orders-in-two-consecutive-years/products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 with cte as
-    (select order_id, product_id, tim, count(order_id) as "total"
-        from
-        (select order_id, product_id, quantity, year(purchase_date) as "tim"
-        from orders) as t1 
-    group by product_id, tim)
+(select product_id, count(order_id) as "total", years
+from
+(select *, year(purchase_date) as "years" from orders) as t1 
+group by product_id, years)
 
-select distinct t2.product_id  from cte as t2, cte as t3 
-where t2.tim - t3.tim = 1 and 
-t2.total >= 3 and t3.total >= 3 and 
-t2.product_id = t3.product_id ; 
+select distinct t2.product_id from cte as t2, cte as t3 
+where t2.product_id = t3.product_id and 
+      t2.total >= 3 and 
+      t3.total >= 3 and 
+      t3.years - t2.years = 1 ;
