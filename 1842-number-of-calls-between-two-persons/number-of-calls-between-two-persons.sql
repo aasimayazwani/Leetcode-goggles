45c1a1667c70c69466d73719ccc444269a63ef36
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-02 22:15:36 -0800

    Time: 1467 ms (13.76%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,12 +1,9 @@
-with cte as
-((select from_id as "person1", to_id as "person2", duration 
-from calls)
-UNION ALL
-(select to_id as "person1", from_id as "person2", duration 
-from calls))
-
-select * from 
-(select person1, person2, count(*) as "call_count",
-sum(duration) as "total_duration" from cte
-group by person1, person2) as t3 
-where person1 < person2 ; 
+select person1, person2, count(*) as "call_count" , sum(duration) as "total_duration"
+from
+((select from_id as "person1", to_id as "person2", duration  from calls)
+UNION ALL 
+(select to_id as "person1", from_id as "person2", duration from calls) )
+as t1 
+where person1 < person2
+group by person1,person2 
+ ;
