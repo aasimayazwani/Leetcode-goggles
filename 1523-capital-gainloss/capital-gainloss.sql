73319846c739e50b729a744f583960d8e3412cd9
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-12 22:15:12 -0700

    Time: 1436 ms (8.69%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,11 +1,13 @@
 # Write your MySQL query statement below
-select stock_name, sum(price) as "capital_gain_loss"
-from
-(select stock_name, 
+with cte as
+(select stock_name,
 case 
     when operation = "Buy" then -price
     when operation = "Sell" then price
 end 
 as "price"
-from stocks) as t1 
-group by stock_name 
+from stocks)
+
+select stock_name, sum(price) as "capital_gain_loss"
+from cte 
+group by stock_name
