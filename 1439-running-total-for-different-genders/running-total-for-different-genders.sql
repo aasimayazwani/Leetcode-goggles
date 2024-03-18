d5e781e8cf369c8270e59f9ff30a67ec87da8f00
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-17 18:44:28 -0700

    Time: 1089 ms (54.67%) | Memory: 0B (100.00%) - LeetSync

--- 1439-running-total-for-different-genders/running-total-for-different-genders.sql
+++ 1439-running-total-for-different-genders/running-total-for-different-genders.sql
@@ -1,5 +1,7 @@
-
-select gender, day,total from 
-(select *, sum(score_points) over (partition by gender order by day) as "total"
-from scores) as t1 
-order by gender asc, day asc ; 
+select gender, day, total from
+((select *, sum(score_points) over (partition by gender order by day) as "total"
+from
+(select gender, day, score_points 
+from scores 
+order by gender asc, day asc) as t1)) as t2 
+order by gender, day   ;
