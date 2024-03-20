a89eaafebf1bd7c1d4083714915b31b7674fd779
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-19 23:33:07 -0700

    Time: 1058 ms (5.17%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,10 +1,9 @@
 select 
-t1.sale_date, 
-sum(t1.sold_num - t2.sold_num) as "diff"
-from 
-sales as t1 , 
+t1.sale_date, t1.sold_num - t2.sold_num as "diff" 
+from sales as t1, 
 sales as t2 
-where t1.fruit = "apples" and t2.fruit = "oranges" 
-and t1.sale_date = t2.sale_date
-group by t1.sale_date
-order by sale_date asc ;
+where 
+t1.fruit = "apples" and 
+t2.fruit = "oranges" and 
+t1.sale_date = t2.sale_date 
+order by sale_date ;
