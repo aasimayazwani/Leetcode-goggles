a3ae4d85726f128a19f0794933f23f893eaa73d9
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-03-27 00:14:26 -0700

    Time: 34 ms (77.95%) | Memory: 16.8 MB (30.88%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def backtrack(current,nums):
-            #print(current)
-            if 0 == len(nums):
-                ans.append(current)
-                return 
+        def back(nums,cur):
+            if len(nums) == 0:
+                ans.append(sorted(cur))
             else:
-                ans.append(current)
+                ans.append(cur)
                 for i in range(0,len(nums)):
-                    backtrack(current + [nums[i]] ,nums[i+1:])
-        backtrack([],nums)
-        print(ans)
-        return ans
-            
-                
+                    back(nums[i+1:],cur+[nums[i]])
+        back(nums,[])
+        return ans 
