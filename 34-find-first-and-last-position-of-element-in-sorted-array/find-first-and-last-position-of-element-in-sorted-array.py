980ae54741879bb95d27abf459e08b9971244d35
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-04-14 19:33:40 -0700

    Time: 86 ms (5.70%) | Memory: 18 MB (9.86%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,39 +1,41 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        current = self.general(nums,target)
-        if current != -1:
-            r_edge = self.rsearch(nums,target,current,len(nums)-1)
-            l_edge = self.lsearch(nums,target,0,current)
-            return [l_edge,r_edge]
-        return [-1,-1]
+        pos = self.general(nums,target)
+        if pos == -1:
+            return [-1,-1]
+        else:
+            l = self.lsearch(nums,0,pos,target)
+            r = self.rsearch(nums,pos,len(nums)-1,target)
+            return [l,r]
         
     def general(self,nums,target):
-        left = 0 
-        right = len(nums)- 1 
+        left, right = 0 ,len(nums)-1
         while left <= right:
-            mid = left + (right-left)//2 
+            mid = left + (right - left)//2
             if target < nums[mid]:
                 right = mid - 1 
-            if target > nums[mid]:
+            elif target > nums[mid]:
                 left = mid + 1 
-            if target == nums[mid]:
+            else:
                 return mid
         return -1
 
-    def rsearch(self,nums,target,left,right):
+    def lsearch(self,nums,left,right,target):
         while left <= right:
-            mid = left + (right-left)//2
+            print(nums[left:right+1])
+            mid = left + (right - left)//2 
+            if target > nums[mid]:
+                left = mid + 1 
+            if target <= nums[mid]:
+                right = mid - 1
+        return left 
+
+    def rsearch(self,nums,left,right,target):
+        while left <= right :
+            print(nums[left:right+1])
+            mid = left + (right-left)//2 
             if target >= nums[mid]:
-                left = mid + 1
+                left = mid + 1 
             if target < nums[mid]:
                 right = mid - 1 
         return right
-
-    def lsearch(self,nums,target,left,right):
-        while left <= right:
-            mid = left + (right-left)//2
-            if target > nums[mid]:
-                left = mid + 1
-            if target <= nums[mid]:
-                right = mid - 1 
-        return left
