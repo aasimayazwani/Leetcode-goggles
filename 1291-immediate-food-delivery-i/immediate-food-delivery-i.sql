aca937b08eff7c370f53484edc0b48a2eca022b1
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-09 06:25:53 -0700

    Time: 607 ms (12.05%) | Memory: 0B (100.00%) - LeetSync

--- 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
+++ 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
@@ -1,2 +1,12 @@
-select round((count(delivery_id)*100)/(select count(*) from delivery),2) as "immediate_percentage" from delivery 
-where order_date = customer_pref_delivery_date ; 
+with cte as 
+(select 
+case
+    when order_date != customer_pref_delivery_date then  0 
+    when order_date = customer_pref_delivery_date then 1
+end 
+as  "total"
+from delivery)
+
+
+select round((sum(total)/count(total))*100,2) as "immediate_percentage"
+from cte 
