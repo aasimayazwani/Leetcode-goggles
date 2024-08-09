6ed2b69964c88259037058ac516381a0d12c83f8
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-09 09:11:33 -0700

    Time: 776 ms (47.05%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,10 +1,13 @@
 # Write your MySQL query statement below
-with cte as (select * from (select account, sum(amount) as "balance"
-from Transactions 
-group by account ) as t1 
-where balance > 10000)
-
-select t1.name, t2.balance from users as t1 
+with cte as
+(select name,account,  amount
+from 
+(select t1.name,t1.account, t2.amount from users as t1 
 inner join 
-cte as t2 
-on t1.account = t2.account ;  
+transactions as t2 
+on t1.account = t2.account) as t3 )
+
+select name as "NAME", sum(amount) as "BALANCE"
+from cte 
+group by name 
+having BALANCE > 10000; 
