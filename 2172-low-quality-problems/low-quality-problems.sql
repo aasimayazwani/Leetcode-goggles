5d87f0ba8a4cd2060bd843795ac8380c4ce05ece
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-08 23:16:23 -0700

    Time: 533 ms (32.43%) | Memory: 0B (100.00%) - LeetSync

--- 2172-low-quality-problems/low-quality-problems.sql
+++ 2172-low-quality-problems/low-quality-problems.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-with cte as 
-(select distinct problem_id from
-(select problem_id, likes, likes + dislikes as "total"
-from problems) as t1 
-where likes/total < 0.6 )
-
-select problem_id from cte 
-order by problem_id asc ; 
+select problem_id from 
+problems 
+where (likes)/(likes+dislikes) < 0.6 
+order by problem_id asc ;
