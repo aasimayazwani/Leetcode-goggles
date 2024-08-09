a16502cecfe6c442b4a04f524d1bffac091ac4a6
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-08 23:25:50 -0700

    Time: 554 ms (26.19%) | Memory: 0B (100.00%) - LeetSync

--- 1892-find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
+++ 1892-find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select event_day as "day", emp_id, sum(out_time-in_time) as "total_time" 
+select event_day as "day", emp_id, sum(out_time - in_time ) as "total_time"
 from employees 
-group by emp_id, event_day ; 
+group by event_day, emp_id ; 
