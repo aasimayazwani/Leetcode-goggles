579eac072a974877c67411c88c0c592d234bf602
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-09 22:51:18 -0700

    Time: 543 ms (9.89%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-select ifnull(max(num),null) as "num"
-from
-(select num, 
-count(num) over (partition by num) as "counting"
-from mynumbers) as t1 
+select ifnull(max(num),null) as "num" from 
+(select *, count(num) over (partition by num) as "counting"
+from mynumbers ) as t1 
 where counting = 1 ; 
