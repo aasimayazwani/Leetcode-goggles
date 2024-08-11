d1ad2acb1a741b4644e2ae8e9c4f394e2355cad1
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-10 19:58:19 -0700

    Time: 273 ms (51.06%) | Memory: 0B (100.00%) - LeetSync

--- 1438-find-the-team-size/find-the-team-size.sql
+++ 1438-find-the-team-size/find-the-team-size.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-select employee_id, count(employee_id) over (partition by team_id) as "team_size"
+select employee_id, team_size from 
+(select *, count(team_id) over (partition by team_id) as "team_size"
 from employee
+) as t1 
