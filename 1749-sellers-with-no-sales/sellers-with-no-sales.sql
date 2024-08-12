e25be945c24b182c3388b96a4fc2d79cdad21a1d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-11 17:57:41 -0700

    Time: 1026 ms (26.21%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,6 +1,7 @@
+# Write your MySQL query statement below
 select seller_name from seller 
-where seller_id not in (
-    select distinct seller_id from orders
-    where year(sale_date) = 2020 
-)
+where seller_id not in
+(select distinct seller_id from orders 
+where 
+date_format(sale_date,"%Y") = "2020")
 order by seller_name asc ; 
