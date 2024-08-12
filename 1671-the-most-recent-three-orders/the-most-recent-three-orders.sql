8a535836b6ee6cbf5d58093949ed98675287fc36
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-11 23:27:13 -0700

    Time: 1485 ms (7.17%) | Memory: 0B (100.00%) - LeetSync

--- 1671-the-most-recent-three-orders/the-most-recent-three-orders.sql
+++ 1671-the-most-recent-three-orders/the-most-recent-three-orders.sql
@@ -1,20 +1,12 @@
-with cte as
-(select 
-t1.name as "customer_name",
-t1.customer_id, 
-t2.order_id, 
-t2.order_date, 
-dense_rank() over (partition by t1.customer_id order by t2.order_date desc) as "ranking"
- from 
-customers as t1 
-inner join 
-orders as t2 
-on t1.customer_id = t2.customer_id )
+with cte as 
+(select customer_id, order_id, order_date from 
+(select *, dense_rank() over (partition by customer_id order by order_date desc) 
+as "ranking" from orders ) as t1 
+where ranking <= 3 )
 
-select customer_name,
-customer_id, 
-order_id, 
-order_date 
-from cte 
-where ranking <= 3
-order by customer_name asc, customer_id asc, order_date desc ;
+select t2.name as "customer_name",
+t3.customer_id, t3.order_id, t3.order_date from customers as t2 
+inner join 
+cte as t3 
+on t2.customer_id = t3.customer_id 
+order by customer_name asc, customer_id asc, order_date desc
