a3d32798ca3871888616f37623680a28a9720904
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-11 22:35:12 -0700

    Time: 504 ms (68.65%) | Memory: 0B (100.00%) - LeetSync

--- 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
@@ -1,11 +1,12 @@
-select transaction_id
-from 
+# Write your MySQL query statement below
+with cte as 
+(select *, dense_rank() over (partition by date order by amount desc) as "ranking"
+from
 (select transaction_id, 
-dense_rank() over (partition by day_value order by amount desc) as "ranking"
-from 
-(select transaction_id, date_format(day,"%Y-%m-%d") as "day_value", 
-amount
-from transactions) as t1) as t2 
-where 
-ranking = 1 
-order by transaction_id asc ;
+    date_format(day,"%Y-%m-%d") as "date", 
+    amount
+    from transactions) as t1) 
+
+select transaction_id from cte 
+where ranking = 1 
+order by transaction_id asc ; 
