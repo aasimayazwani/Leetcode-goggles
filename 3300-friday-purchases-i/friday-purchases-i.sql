6c0d88e6bb9c8ef72402b58e2f7c7562f1d5c915
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-13 08:26:22 -0700

    Time: 389 ms (61.11%) | Memory: 0B (100.00%) - LeetSync

--- 3300-friday-purchases-i/friday-purchases-i.sql
+++ 3300-friday-purchases-i/friday-purchases-i.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
 select week_of_month, purchase_date, sum(amount_spend) as "total_amount"
-from 
-(select 
-round(day(purchase_date)/7,0) +1 as "week_of_month",
-date_format(purchase_date,"%W") as "day_",
-purchase_date,
-amount_spend 
-from 
-purchases 
-having day_ = "Friday") as t1
-group by week_of_month 
-order by week_of_month asc ; 
+    from 
+    (select ceil(days/7) as "week_of_month",
+    purchase_date, 
+    amount_spend 
+    from
+        (select *, 
+        date_format(purchase_date,"%d") as "days",
+        date_format(purchase_date,"%W") as "weekday" from purchases) as t1
+    where weekday = "Friday") as t2 
+    group by week_of_month
+    order by week_of_month asc ; 
