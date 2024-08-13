bc0a03cfdd323617c05924d261c53034308b1c69
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-12 22:28:39 -0700

    Time: 782 ms (61.56%) | Memory: 0B (100.00%) - LeetSync

--- 580-count-student-number-in-departments/count-student-number-in-departments.sql
+++ 580-count-student-number-in-departments/count-student-number-in-departments.sql
@@ -1,13 +1,10 @@
 with cte as 
 (select dept_id, count(distinct student_id) as "student_number"
 from student
-group by dept_id )
+group by dept_id)
 
-
-select  
-t1.dept_name, ifnull(t2.student_number,0) as "student_number" 
-from department as t1 
+select t1.dept_name, ifnull(t2.student_number,0) as "student_number" from department as t1 
 left join 
 cte as t2 
-on t1.dept_id = t2.dept_id 
-order by t2.student_number desc, t1.dept_name asc ;
+on t1.dept_id = t2.dept_id
+order by t2.student_number desc, t1.dept_name asc ; 
