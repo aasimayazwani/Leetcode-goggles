5674287fa73dd2c4d1c3b649e86cb6147ee22108
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-12 22:53:14 -0700

    Time: 490 ms (34.03%) | Memory: 0B (100.00%) - LeetSync

--- 2252-the-airport-with-the-most-traffic/the-airport-with-the-most-traffic.sql
+++ 2252-the-airport-with-the-most-traffic/the-airport-with-the-most-traffic.sql
@@ -1,13 +1,11 @@
-# Write your MySQL query statement below
 with cte as 
-(select airport_id, sum(flights_count) as "total"
-from 
-(select departure_airport as "airport_id", flights_count from flights
-union all 
-select arrival_airport as "airport_id", flights_count from flights) as r1 
-group by airport_id) 
+(select airport_id, sum(flights_count) as "counting" from
+((select departure_airport as "airport_id", flights_count from flights)
+UNION ALL
+(select arrival_airport as "airport_id", flights_count from flights)) as t1 
+group by airport_id)
 
 select airport_id from
-(select *, dense_rank() over (order by total desc) as "ranking" from cte )
-as t1 
-where ranking = 1 ;
+(select *, dense_rank() over (order by counting desc) as 'ranking'
+from cte  ) as t4 
+where ranking = 1 ; 
