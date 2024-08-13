8d40e26c202d68287a0cd175bd6676f129c43f93
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-13 08:45:40 -0700

    Time: 649 ms (9.52%) | Memory: 0B (100.00%) - LeetSync

--- 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
+++ 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
@@ -1,14 +1,10 @@
 with cte as 
-(select task_id, date_format(submit_date,"%W") as "day_name" 
-from tasks) 
+(select *, date_format(submit_date,"%W") as "weekday" from tasks)
 
-select sum(day_value) as "weekend_cnt",
-(select count(*) from tasks) - sum(day_value) as "working_cnt"
- from 
-(select task_id, 
-case 
-    when day_name IN ("Saturday","Sunday") then 1 
-    else 0 
-    end 
-as "day_value"
-from cte ) as t1 
+
+select count(distinct t1.task_id) as "weekend_cnt",
+        count(distinct t2.task_id) as "working_cnt"
+        from cte as t1, 
+        cte as t2
+        where t1.weekday in ("Saturday","Sunday") and 
+        t2.weekday not in ("Saturday","Sunday") ; 
