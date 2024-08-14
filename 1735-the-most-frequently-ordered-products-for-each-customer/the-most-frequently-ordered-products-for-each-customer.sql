0a297401a5e60c41d15edc5ba4802544f6c8f161
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-14 08:31:10 -0700

    Time: 2830 ms (10.85%) | Memory: 0B (100.00%) - LeetSync

--- 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
+++ 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,12 +1,19 @@
+# Write your MySQL query statement below
 with cte as 
-(select *, dense_rank() over (partition by customer_id order by total desc ) as "ranking"
-from (select customer_id, product_id, count(product_id) as "total"
-from orders 
-group by customer_id, product_id ) as t1 )
+(select customer_id, 
+        product_id, 
+        dense_rank() over (partition by customer_id order by total desc) as "ranking"
+        from
+        (select customer_id, product_id, count(*) as "total"
+        from orders
+        group by customer_id, product_id) as t1 )
 
-
-select t2.customer_id, t2.product_id, t1.product_name from cte as t2 
+select 
+t2.customer_id, 
+t2.product_id, 
+t3.product_name
+from cte as t2 
 inner join 
-products as t1 
-on t1.product_id = t2.product_id 
-where ranking = 1 ;
+products as t3 
+on t2.product_id = t3.product_id 
+where t2.ranking = 1 
