be98bd1d468bc868218669dcac0f70d3dd85dbc6
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-14 22:44:59 -0700

    Time: 443 ms (33.93%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
-select distinct author_id as "id" 
-from 
-views 
-where author_id = viewer_id 
-order by author_id asc 
+select distinct author_id as "id"
+from
+(select author_id 
+from views 
+where author_id = viewer_id) as t1 
+order by id asc; 
