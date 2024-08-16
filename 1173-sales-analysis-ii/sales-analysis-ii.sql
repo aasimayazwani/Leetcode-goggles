6f36c1ebed8e25e90814d86a5dca8c15525b53f8
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-16 07:02:44 -0700

    Time: 1911 ms (5.08%) | Memory: 0B (100.00%) - LeetSync

--- 1173-sales-analysis-ii/sales-analysis-ii.sql
+++ 1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -1,11 +1,14 @@
-# Write your MySQL query statement below
-with cte as (select t2.buyer_id, t1.product_name from product as t1 
-inner join
-sales as t2 on 
- t1.product_id = t2.product_id )
+with cte as
+(select t2.buyer_id, 
+        t1.product_name 
+        from product as t1 
+        inner join 
+        sales as t2 
+        on t1.product_id = t2.product_id )
 
-
-select distinct buyer_id from cte where 
-cte.product_name = "S8" 
-and cte.buyer_id not in (select buyer_id from cte where 
-cte.product_name = "iPhone" )
+select distinct t4.buyer_id from cte as t4 , cte as t5 
+where t4.buyer_id = t5.buyer_id and 
+t4.product_name = "S8" and t5.product_name != "iPhone" and 
+        t4.buyer_id not in (select t4.buyer_id from cte as t4 , cte as t5 
+        where t4.buyer_id = t5.buyer_id and 
+        t4.product_name = "S8" and t5.product_name = "iPhone")
