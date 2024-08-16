823bddde7f9ba88e41b44a746b11cf617619e8b4
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-16 09:17:44 -0700

    Time: 589 ms (46.19%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
-select employee_id,
-case when left(name,1) != "M" and employee_id%2 != 0 then salary
-else 0 
-end as "bonus"
-from Employees
-order by employee_id asc  ; 
+select employee_id, bonus from
+    (select employee_id, 
+        case 
+        when employee_id % 2 != 0 and left(name,1) != "M" then salary
+        else 0 
+        end 
+        as "bonus"
+    from employees) as t1 
+order by employee_id asc ; 
