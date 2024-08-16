e67c3b530191e0561cdc28ea1429bd4685781d33
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-15 21:47:39 -0700

    Time: 938 ms (45.09%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -1,19 +1,19 @@
-# Write your MySQL query statement below
-with cte as 
-(select  
-sum(t1.price*t2.quantity) as "total",
-t2.customer_id , 
-date_format(t2.order_date,"%Y-%m") as "time" 
-from product as t1 
-inner join orders as t2 
-on t1.product_id = t2.product_id 
-group by t2.customer_id, time )
+with cte as
+    (select
+    t1.product_id, t2.customer_id, 
+    date_format(t2.order_date,"%Y-%m") as "month",
+    sum(t1.price*t2.quantity) as "total"
+    from product as t1 
+    inner join 
+    orders as t2 
+    on t1.product_id = t2.product_id 
+    group by t2.customer_id, month)
 
-select df1.customer_id, df1.name  from customers as df1 
-inner join 
-(select distinct t1.customer_id from cte as t1, cte as t2 
-where t1.customer_id = t2.customer_id 
-and 
-t1.time = "2020-06" and t2.time = "2020-07"  and 
-t1.total >= 100 and t2.total >= 100 ) as df2 
-on df1.customer_id = df2.customer_id ; 
+select t4.customer_id, t3.name  from customers as t3 
+    inner join 
+    (select t1.customer_id  
+    from cte as t1, cte as t2 
+    where t1.customer_id = t2.customer_id and 
+    t1.total >= 100 and t2.total >= 100  and 
+    t1.month = "2020-06" and t2.month = "2020-07") as t4 
+    on t3.customer_id = t4.customer_id ; 
