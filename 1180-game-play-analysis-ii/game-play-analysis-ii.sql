ab787d2bc9738299a6f2c36e14dcef33c1aeb898
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-15 23:15:43 -0700

    Time: 522 ms (50.20%) | Memory: 0B (100.00%) - LeetSync

--- 1180-game-play-analysis-ii/game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/game-play-analysis-ii.sql
@@ -1,7 +1,10 @@
-with cte as 
-(select *, dense_rank() over (partition by player_id order by event_date asc ) 
-as "ranking"
-from activity ) 
+with cte as
+(select *, dense_rank() over (partition by player_id order by smallest asc) as "ranking"
+from
+(select player_id, device_id, min(event_date) as "smallest"
+from activity
+group by player_id, device_id) as t1 
+)
 
-select player_id, device_id from cte
-where ranking = 1 
+select player_id, device_id from cte 
+where ranking = 1 ; 
