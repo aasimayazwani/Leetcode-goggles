90f5e5358a2ce0607c3922ee62ad8509a03887ca
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-16 19:10:44 -0700

    Time: 433 ms (94.50%) | Memory: 0B (100.00%) - LeetSync

--- 1948-rearrange-products-table/rearrange-products-table.sql
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -1,10 +1,6 @@
-# Write your MySQL query statement below
-with cte as 
-(select product_id,"store1" as store, store1 as "price" from products
-uNION ALL
-select product_id,"store2" as store, store2 as "price" from products 
-uNION ALL
-select product_id,"store3" as store, store3 as "price" from products )
-
-select * from cte 
-where price is not null ; 
+select * from 
+(select product_id, "store1" as "store" , store1 as "price" from products UNION 
+ALL
+select product_id, "store2" as "store" , store2 as "price" from products UNION ALL
+select product_id, "store3" as "store" , store3 as "price" from products ) as t1 
+where price is not Null ; 
