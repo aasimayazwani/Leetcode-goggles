bb71027e165ef77e3d0879e16ce70008f073cce9
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-17 18:19:22 -0700

    Time: 715 ms (90.79%) | Memory: 0B (100.00%) - LeetSync

--- 1958-ad-free-sessions/ad-free-sessions.sql
+++ 1958-ad-free-sessions/ad-free-sessions.sql
@@ -1,8 +1,11 @@
-with cte as (select distinct t1.session_id from playback as t1 
-inner join 
-ads as t2 
-on t1.customer_id = t2.customer_id 
-where t2.timestamp >= t1.start_time and t2.timestamp <= t1.end_time)
-
-select session_id from playback where
-session_id not in (select session_id from cte); 
+select session_id from playback 
+where 
+session_id not in
+    (select t1.session_id
+    from  ads as t2 
+    left join 
+    playback as t1 
+    on 
+    t1.customer_id = t2.customer_id and 
+    t2.timestamp >= t1.start_time and 
+    t2.timestamp <= t1.end_time) ;
