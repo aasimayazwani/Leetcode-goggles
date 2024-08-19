e644c96e7ae887051bdd303405e705649b83e470
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-18 22:00:39 -0700

    Time: 574 ms (80.71%) | Memory: 0B (100.00%) - LeetSync

--- 2014-orders-with-maximum-quantity-above-average/orders-with-maximum-quantity-above-average.sql
+++ 2014-orders-with-maximum-quantity-above-average/orders-with-maximum-quantity-above-average.sql
@@ -1,11 +1,9 @@
-# Write your MySQL query statement below
-with temp_table as 
-(select distinct order_id, 
-        avg(quantity) over 
-        (partition by order_id) as avg_quantity,
-        max(quantity) over 
-        (partition by order_id) as max_quantity 
-        from OrdersDetails) 
-        
-select order_id  from temp_table
-where max_quantity > (select max(avg_quantity) from temp_table) ;
+with cte as 
+(select order_id, product_id, 
+        avg(quantity) over (partition by order_id) as "avg_quantity", 
+        max(quantity) over (partition by order_id) as "max_quantity"
+        from 
+        ordersdetails)
+
+select distinct order_id  from cte
+where max_quantity > (select max(avg_quantity) from cte) ;
