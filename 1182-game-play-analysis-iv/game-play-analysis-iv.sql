25d11c7c5614093409049195b151c1ca98c51649
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-18 22:22:27 -0700

    Time: 500 ms (88.43%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -2,12 +2,14 @@
 # Algorithm: 
     ## for each player identify the earliest login date 
     ## identify those player_id where the event_date 
+with cte as
+    (select *, dense_rank() over (partition by player_id order by event_date asc) as "ranking" from activity
+    ),
 
-select round(fraction,2) as "fraction" from 
-(select count(distinct player_id)/(select count(distinct player_id) from activity)
-as "fraction" from 
-(select *, min(event_date) over (partition by player_id) as "earliest"
-from activity ) 
-as t1 
-where datediff(event_date,earliest) = 1 ) as t2 
-;
+table1 as
+(select distinct t1.player_id from cte as t1, cte as t2 
+where t1.player_id = t2.player_id and 
+t1.ranking = 1 and t2.ranking = 2 and 
+abs(datediff(t2.event_date,t1.event_date)) = 1) 
+
+select round(count(player_id)/(select count(distinct player_id) from cte),2) as "fraction" from table1 
