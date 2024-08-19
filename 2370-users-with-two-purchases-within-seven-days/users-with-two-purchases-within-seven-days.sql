7f32342a439b366cc3040ccd4711aabbe174dfd1
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-18 20:00:55 -0700

    Time: 368 ms (86.57%) | Memory: 0B (100.00%) - LeetSync

--- 2370-users-with-two-purchases-within-seven-days/users-with-two-purchases-within-seven-days.sql
+++ 2370-users-with-two-purchases-within-seven-days/users-with-two-purchases-within-seven-days.sql
@@ -1,9 +1,8 @@
 # Write your MySQL query statement below
-select distinct t2.user_id 
-from purchases as t1 , purchases as t2 
-where t1.purchase_id !=  t2.purchase_id and 
-t1.user_id = t2.user_id and 
-t2.purchase_date >= t1.purchase_date  
-and  
-datediff(t2.purchase_date,t1.purchase_date) <= 7
-order by user_id  ; 
+select distinct t1.user_id from purchases as t1, 
+    purchases as t2 
+    where 
+    t1.purchase_id != t2.purchase_id and 
+    t1.user_id = t2.user_id and 
+    abs(datediff(t2.purchase_date,t1.purchase_date)) <= 7 
+    order by user_id asc ; 
