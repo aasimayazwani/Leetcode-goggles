b12c780ce6d458c7dbb5d692a6535d48a815863a
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-18 19:15:27 -0700

    Time: 729 ms (21.72%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,13 +1,15 @@
-# Write your MySQL query statement below
-with cte as 
-(select * from 
-(select *, dense_rank() over (partition by customer_id order by order_date asc) as 
-"counting"
-from delivery) as t1 
-where counting = 1 )
+# first orders of customers that are also delievred in the 
+# same day as the order was placed
+with cte as
+(select customer_id, 
+case 
+    when order_date = customer_pref_delivery_date then 1 
+    else 0 
+    end 
+    as "status", 
+    dense_rank() over (partition by customer_id order by order_date asc)
+    as "ranking"
+    from delivery)
 
-
-select round(count(delivery_id)*100/(select count(delivery_id) from cte),2)
-as "immediate_percentage"
-from cte  
-where order_date = customer_pref_delivery_date ; 
+select round(100*(sum(status)/count(*)),2) as "immediate_percentage" from cte 
+where ranking = 1 ; 
