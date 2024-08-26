c283f297d18a660b7bb665cc2afae785b1a5614a
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-08-25 22:05:39 -0700

    Time: 536 ms (39.56%) | Memory: 0B (100.00%) - LeetSync

--- 3097-total-traveled-distance/total-traveled-distance.sql
+++ 3097-total-traveled-distance/total-traveled-distance.sql
@@ -1,10 +1,8 @@
-with cte as
-(select user_id, sum(distance) as "travelled_distance"
-from rides 
-group by user_id) 
-
-select distinct t1.user_id , t1.name ,ifnull(t2.travelled_distance,0) as 'traveled distance' from users as t1 
+select 
+t1.user_id, t1.name, ifnull(sum(t2.distance),0) as "traveled distance"
+ from users as t1 
 left join 
-cte as t2 
-on t1.user_id = t2.user_id
-order by t1.user_id asc  ; 
+rides as t2 
+on t1.user_id = t2.user_id 
+group by t1.user_id 
+order by user_id asc ; 
