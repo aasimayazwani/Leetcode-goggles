b9a94bec4cd5dcbf8a8ca69903136923e79a9479
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-13 16:41:49 -0700

    Time: 26 ms (95.84%) | Memory: 16.5 MB (37.70%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,31 +1,27 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        # split the string s based on the empty spaces in s
-        # use a hashmap to keep track of each pattern[index] -> s[index], if the key doesn't always exist it doesn't matter, 
-        # but, if the key does exist already then the new value
-        # must match the aready existing value pass, otherwise return false. 
-        # if no failure is found return true. 
-        mapping = {}
+    def wordPattern(self, pattern, s):
         s = s.split(" ")
         if len(s) != len(pattern):
             return False
-            
-        status1 = self.check(pattern,s)
-        status2 = self.check(s,pattern)
-        answer= [status1,status2]
-        if False in answer:
-            return False 
-        return True 
+        mapping ={}
+        first_check = True
+        for i in range(0,len(pattern)):
+            current = pattern[i]
+            if current not in mapping:
+                mapping[current] = s[i]
+            else:
+                if mapping[current] != s[i]:
+                    first_check = False
+                    break
         
-
-    def check(self,s,t):
-        mapping = {}
+        second = {}
+        second_check = True
         for i in range(0,len(s)):
-            if s[i] in mapping:
-                if mapping[s[i]] == t[i]:
-                    pass 
-                else:
-                    return False
-            if s[i] not in mapping:
-                mapping[s[i]] = t[i]
+            if s[i] not in second:
+                second[s[i]] = pattern[i]
+            else:
+                if second[s[i]] != pattern[i]:
+                    second_check = False
+        if False in [second_check,first_check]:
+            return False
         return True
