c30ac20ffef19414162bb22c43cfec82f0987dc7
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-13 11:41:09 -0700

    Time: 37 ms (44.07%) | Memory: 16.5 MB (72.21%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,22 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        # EDGE CASES
-        # if len(word1) > 0 and len(word2) > 0 
-        # if len(word1) ==0 and len(word2) > 0 
-        # if len(word2) == 0  and len(word1) > 0 
-        
-        # we will create a pointer for each of the words, word1[[pointer]] + word2[pointer]
-        # move them after each addition
-        # stop the process if either of the strings are of zero length 
-
         answer = ""
-        while True:
+
+        edge1 = len(word1) - 1
+        pointer1 = 0 
+
+        edge2 = len(word2) - 1
+        pointer2 = 0
+
+        while (pointer1 <= edge1) and (pointer2 <= edge2) :
             if len(word1) > 0:
-                answer += word1[0]
-                word1 = word1[1:]
+                answer += word1[pointer1]
+                pointer1 +=1
             if len(word2) > 0:
-                answer += word2[0]
-                word2 = word2[1:]
-            else:
-                break
-        return answer + word1 + word2
+                answer += word2[pointer2]
+                pointer2 +=1
+
+        return answer + word1[pointer1:] + word2[pointer2:]
+        
