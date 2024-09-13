50203e424659c15c3c7dffc5fcabef5671d9108b
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-13 11:24:19 -0700

    Time: 175 ms (22.48%) | Memory: 20.8 MB (29.58%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,17 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        left = 0 
-        right = len(s)-1
+        left, right = 0, len(s)-1
         while left <= right:
-            # left < right for the even sized length 
-            # left <= right for the odd sized length
-            temp = s[right]
-            s[right] = s[left]
-            s[left] = temp 
-            left +=1 
-            right -=1 
-
-        
+            s[left], s[right] = s[right], s[left]
+            left +=1
+            right -=1
+    
+            
