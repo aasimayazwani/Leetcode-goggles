b69c7ca5ada4b6b175e9ffe9401dcf42d5e5a5fa
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-13 11:35:46 -0700

    Time: 40 ms (20.55%) | Memory: 16.4 MB (95.62%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -9,12 +9,14 @@ class Solution:
         # move them after each addition
         # stop the process if either of the strings are of zero length 
 
-        pointer1 = 0 
-        pointer2 = 0 
-        result = ""
-        while (pointer1 < len(word1)) and (pointer2  <len(word2)):
-            result += word1[pointer1] + word2[pointer2]
-            pointer1 +=1 
-            pointer2 +=1 
-        result += word1[pointer1:] + word2[pointer2:]
-        return result 
+        answer = ""
+        while True:
+            if len(word1) > 0:
+                answer += word1[0]
+                word1 = word1[1:]
+            if len(word2) > 0:
+                answer += word2[0]
+                word2 = word2[1:]
+            else:
+                break
+        return answer + word1 + word2
