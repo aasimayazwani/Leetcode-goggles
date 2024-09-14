bc0bddf7a503cadf751dd86763f8fbee9cba14ec
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-14 14:02:51 -0700

    Time: 137 ms (67.75%) | Memory: 28.6 MB (5.78%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        from collections import Counter
+        mapping = Counter(nums)
+        return [item[0] for item in mapping.items() if item[1] == 1][0]
