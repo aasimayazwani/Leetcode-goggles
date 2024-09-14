0a8b89e047be83aea74e37b85db73e788a0fe608
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-14 08:09:40 -0700

    Time: 165 ms (80.89%) | Memory: 18.1 MB (21.18%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,15 +4,10 @@ class Solution:
         # then use hashmpa items to search for items which occur greater then n/2 
         # the moment you find them stop, searching since only 
         # one element can occure n/2 times. 
-        import math
-        length = math.ceil(len(nums)/2)
-        mapping = {}
-        for i in range(0,len(nums)):
-            if nums[i] in mapping:
-                mapping[nums[i]] +=1 
-            if nums[i] not in mapping:
-                mapping[nums[i]] = 1
-        pairs = list(mapping.items())
-        for key, value in pairs:
-            if value >= length:
-                return key
+        import heapq
+        from collections import Counter
+        mapping = Counter(nums)
+        pairs = [(-value,key) for (key, value) in mapping.items()]
+        heapq.heapify(pairs)
+        value, key = heapq.heappop(pairs)
+        return key
