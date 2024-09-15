a7d5648f8b754c6f7aab44d9e7616024526575cc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-14 21:43:11 -0700

    Time: 135 ms (5.06%) | Memory: 17.2 MB (11.93%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,19 +1,22 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        import math
+        tokens = [item for item in tokens]
         mapping = {"+":1,"-":1,"*":1,"/":1}
         stack = []
-        for i in range(0,len(tokens)):
-            if len(stack) <2:
-                stack.append(tokens[i])
-            else:
-                #print(stack)
-                if tokens[i] in mapping:
-                    operation = stack[-2] + tokens[i] + stack[-1]
-                    value = int(eval(operation))
+        while len(tokens) > 0:
+            #print(stack)
+            if len(stack) < 2:
+                stack.append(tokens[0])
+                tokens.pop(0)
+            else :
+                if tokens[0] in mapping:
+                    operation = stack[-2] + tokens[0] + stack[-1]
+                    tokens.pop(0)
                     stack.pop(-1)
                     stack.pop(-1)
-                    stack.append(str(value))
+                    stack.append(str(int(eval(operation))))
                 else:
-                    stack.append(tokens[i])
-        
+                    stack.append(str(tokens[0]))
+                    tokens.pop(0)
         return int(stack[0])
