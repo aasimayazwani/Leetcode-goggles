4727291a16eff3182d076e4b592290fcac09d9fc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-14 17:24:11 -0700

    Time: 43 ms (92.63%) | Memory: 16.5 MB (83.93%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,12 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        left = 0 
-        right = len(x) - 1
+        left, right = 0 , len(x)-1
         while left <= right:
-            if x[left] == x[right]:
-                left +=1 
-                right -=1
-            else:
+            if x[left] != x[right]:
                 return False 
-        return True
+            left +=1 
+            right -=1 
+        return True 
