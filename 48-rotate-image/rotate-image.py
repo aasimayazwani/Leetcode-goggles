b635f0a40209f0c23e3d02410bd721f412cfc8b6
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-14 23:19:44 -0700

    Time: 38 ms (59.22%) | Memory: 16.6 MB (46.63%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -6,10 +6,7 @@ class Solution:
         k = len(matrix[0])
         for i in range(0,k):
             matrix.append([item[i] for item in matrix[0:k]][::-1][0:k])
-            #print(current)
-            #matrix.append(current[0:k])
-
-        
+            
         while k > 0:
             matrix.pop(0)
             k-=1
