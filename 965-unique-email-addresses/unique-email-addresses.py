e11da9daf7f226c99a9a2d685bf94d5986cc1781
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-15 09:01:16 -0700

    Time: 56 ms (52.36%) | Memory: 16.7 MB (11.70%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -1,20 +1,15 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        # remove all the text after + 
-        # replace the 
-        # remove the last 4 letter since they will be a problematic
-
-        emails  = [item[0:-4] for item in emails]
-        answered= set()
+        emails = [item[:-4] for item in emails]
+        valid = set()
         for i in range(0,len(emails)):
-            #print(emails[i])
-            details, domain = emails[i].split("@")
-            details = details.replace(".","")
-            if "+" in details:
-                details = details.split("+")[0:-1]
-                merged = details[0] + "@" + domain
-            else:
-                merged = details + "@" + domain
-        
-            answered.add(merged)
-        return len(answered)
+            first, second = emails[i].split("@")
+            first = self.localname(first)
+            current = "@".join([first,second])
+            valid.add(current)
+        return len(list(valid))
+
+    def localname(self,name):
+        name = name.replace(".","")
+        name = name.split("+")[0]
+        return name 
