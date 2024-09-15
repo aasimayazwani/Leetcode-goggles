d4a4b29676e00ed12ac26b672189cd9873c649cb
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-15 00:41:38 -0700

    Time: 537 ms (6.27%) | Memory: 46.9 MB (5.07%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,41 +1,38 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        ans = []
-        def backtrack(s,cur):
-            if len(s) == 0:
-                ans.append(cur.split(","))
-            else:
-                backtrack(s[1:],cur +s[0])
-                if len(cur) > 0:    
-                    backtrack(s[1:],cur +","+s[0])
-        backtrack(s,"")
-        result = []
-        for i in range(0,len(ans)):
-            if self.gen(ans[i]):
-                result.append(ans[i])
-        return result 
-
-    def valid(self,s):
-        left = 0 
-        right = len(s) - 1 
-        while left <= right:
-            if s[left] != s[right]:
-                return False, s
-            else:
-                pass
-            left +=1 
-            right -=1
-        return True, s
-
-    def gen(self,arr):
-        for i in range(0,len(arr)):
-            if len(arr[i]) > 1:
-                validity, string = self.valid(arr[i])
-                if validity == False:
+        def check(s):
+            left, right = 0 , len(s)-1
+            while left <= right:
+                if s[left] != s[right]:
                     return False
-                else:
-                    pass
-        return True
+                left +=1 
+                right -=1 
+            return True
 
-s = "aab"
 
+        def checking(arr):
+            for i in range(0,len(arr)):
+                if len(arr[i]) ==1:
+                    pass 
+                if len(arr[i]) > 1:
+                    #print(arr[i])
+                    if check(arr[i]) == False:
+                        return False
+            return True
+
+        ans = []        
+        def total(current, s):
+            if len(s) ==0:
+                ans.append(current.split("."))
+            else:
+                if len(current) > 0:
+                    total(current + "." + s[0], s[1:])
+                total(current + s[0], s[1:])
+        total("",s)
+
+        answer = []
+        for i in range(0,len(ans)):
+            if checking(ans[i]):
+                answer.append(ans[i])
+        return answer 
+        
