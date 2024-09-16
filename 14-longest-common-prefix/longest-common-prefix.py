c6bbf936d4121bb2f4125c6a0a9a508118b75c5a
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-16 00:08:19 -0700

    Time: 34 ms (79.43%) | Memory: 16.7 MB (30.28%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,21 +1,11 @@
 class Solution:
     def longestCommonPrefix(self, strs): 
-        if len(set(strs)) == 1:
-            return strs[0]
-
-        result = []
+        answer = ""
         minimum = min([len(item) for item in strs])
-        print(minimum)
-        for length in range(1,minimum+1):
-            mapping = {}
-            for i in range(0,len(strs)):
-                current = strs[i][0:length]
-                if current not in mapping:
-                    mapping[current] = 1 
-                
-            #print(mapping)
-            if len(mapping) == 1:
-                result.append(list(mapping.keys())[0])
-        if len(result) == 0:
-            return ""
-        return result[-1]
+        for i in range(0,minimum):
+            array = [item[i] for item in strs]
+            if (len(set(array)) == 1) and (len(array[0]) > 0):
+                answer += array[0]
+            else:
+                break
+        return answer 
