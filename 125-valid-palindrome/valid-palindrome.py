e9060ce83f742fd7bd844990ab233383e5a6dece
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-09-24 23:02:28 -0700

    Time: 40 ms (81.91%) | Memory: 21.8 MB (7.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = [item.lower() for item in s if item.isalpha() or item.isdigit()]
+        s = [item.lower() for item in s if item.isalpha() is True or item.isdigit() is True]
         left, right = 0, len(s)-1
         while left < right:
             if s[left] != s[right]:
                 return False 
             else:
-                left +=1
+                left +=1 
                 right -=1 
-
         return True 
