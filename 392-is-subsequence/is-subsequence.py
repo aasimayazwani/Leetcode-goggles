12769e1cbc60a0163a4a2d634174381abaabcf12
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-10-12 13:51:08 -0700

    Time: 33 ms (74.76%) | Memory: 16.7 MB (6.57%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -2,14 +2,11 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         s = [item for item in s]
         t = [item for item in t]
-        # if the first element of t matches the first element of s, 
-        # delete these elements, but if they don't match pop the first element of t 
-        # keep on doing this until matches are found 
-        # stop the process when either the s or t have length of zero. 
-        while (len(s) != 0) and (len(t) != 0):
-            if s[0] == t[0]:
+        if len(s) == 0 :
+            return True 
+        for i in range(0,len(t)):
+            if t[i] == s[0]:
                 s.pop(0)
-                t.pop(0)
-            elif s[0] != t[0]:
-                t.pop(0)
+            if len(s) == 0:
+                return True
         return len(s) == 0 
