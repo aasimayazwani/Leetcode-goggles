6c7d56484c8b91ff95df39bc4d4109ca607bc16b
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-10-17 17:01:34 -0700

    Time: 14 ms (100.00%) | Memory: 19.1 MB (99.95%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,14 +1,20 @@
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals = intervals + [newInterval]
+    def insert(self, intervals, newInterval):
+        intervals += [newInterval]
         intervals = sorted(intervals,key=lambda x:x[0])
-        stack = []
-        for i in range(0,len(intervals)):
+        print(intervals)
+        stack= []
+        while intervals:
             if len(stack) == 0:
-                stack.append(intervals[i])
+                stack.append(intervals[0])
+                intervals.pop(0)
             else:
-                if intervals[i][0] <=  stack[-1][1]:
-                    stack[-1][1] = max(intervals[i][1],stack[-1][1])
+                if stack[-1][1] >= intervals[0][0]:
+                    previous = stack.pop(-1)
+                    new = [previous[0],max(previous[1],intervals[0][1])]
+                    intervals.pop(0)
+                    stack.append(new)
                 else:
-                    stack.append(intervals[i])
+                    stack.append(intervals[0])
+                    intervals.pop(0)
         return stack
