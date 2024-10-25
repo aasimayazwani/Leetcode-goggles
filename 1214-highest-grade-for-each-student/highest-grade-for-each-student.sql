4103db8c53b56dfec8def40d03f0a1de8d5d0d64
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-10-24 22:49:19 -0700

    Time: 580 ms (30.52%) | Memory: 0B (100.00%) - LeetSync

--- 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
+++ 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -1,7 +1,7 @@
-select student_id, course_id, grade
-from 
-(select *, 
-dense_rank() over (partition by student_id order by grade desc, course_id asc ) as "ranking"
-from enrollments) as t1 
-where ranking=1
-order by student_id asc ;
+with cte as 
+(select *, dense_rank() over (partition by student_id order by grade desc, course_id asc ) as "ranking"
+from enrollments)
+
+select student_id, course_id, grade from cte 
+where ranking = 1 
+order by student_id asc ; 
