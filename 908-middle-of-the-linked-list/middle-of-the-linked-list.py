5d8a3396089305eb0b42ff3db9485c357451ab95
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-10-26 07:33:41 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (7.73%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,17 +5,10 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
+        slow, fast = head, head 
         count = 0
-        temp = head
-        while head:
-            count += 1 
-            arr.append(head.val)
-            head = head.next # makes sures the nodes keep on moving 
-        middle = count//2 + 1  
-        print(middle)
-        counting = 0 
-        while counting < middle-1 : #we'll stop once count = middle
-            counting+=1 #until then, we increase count by one
-            temp = temp.next #as we go to the next value
-        return temp
+        while fast and fast.next :
+            slow = slow.next 
+            fast = fast.next.next
+
+        return slow 
