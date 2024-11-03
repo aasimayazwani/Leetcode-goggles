26904fb0cf6aa6cf8b39ea2d1b9a0fb3c58c1768
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-03 14:40:51 -0800

    Time: 7 ms (56.93%) | Memory: 18.3 MB (7.38%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,29 +1,26 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        parents = {}
-        for i in range(0,len(isConnected)):
-            parents[i]= i
         edges = []
         for i in range(0,len(isConnected)):
-            for j in range(0,len(isConnected)):
-                if i!=j and isConnected[i][j] == 1:
+            for j in range(i+1,len(isConnected)):
+                if isConnected[i][j] == 1:
                     edges.append([i,j])
+    
+        parent = {}
+        for i in range(0,len(isConnected)):
+            parent[i] = i
 
-        def find(x):
-            if parents[x] != x:
-                parents[x] = find(parents[x])
-            else:
-                pass 
-            return parents[x]
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
 
-        def union(rootx,rooty):
-            rootx, rooty = find(rootx), find(rooty)
-            if rootx != rooty:
-                parents[rooty] = rootx
+        def union(nodex,nodey):
+            nodex, nodey = find(nodex), find(nodey)
+            if nodex != nodey:
+                parent[nodey] = nodex  
+            
+        for x, y  in edges:
+            union(x,y)
 
-
-        for u, v in edges:
-            union(u,v)
-
-        unique_components = len(set(find(i) for i in range(len(isConnected))))
-        return unique_components
+        return len(set([find(item) for item in range(len(isConnected))]))
