a81cf5a8899d82185f0f153e785498e54a98d4bc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-04 23:29:45 -0800

    Time: 7 ms (56.95%) | Memory: 18.2 MB (10.18%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,26 +1,25 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def find(nodex):
+            if parent[nodex] != nodex:
+                parent[nodex] = find(parent[nodex])
+            return parent[nodex]
+
+        def union(nodex, nodey):
+            nodex, nodey = find(nodex), find(nodey)
+            if nodex != nodey:
+                parent[nodey] = nodex
         edges = []
         for i in range(0,len(isConnected)):
             for j in range(i+1,len(isConnected)):
                 if isConnected[i][j] == 1:
                     edges.append([i,j])
-    
         parent = {}
-        for i in range(0,len(isConnected)):
+        for i in range(len(isConnected)):
             parent[i] = i
 
-        def find(node):
-            if parent[node] != node:
-                parent[node] = find(parent[node])
-            return parent[node]
-
-        def union(nodex,nodey):
-            nodex, nodey = find(nodex), find(nodey)
-            if nodex != nodey:
-                parent[nodey] = nodex  
-            
-        for x, y  in edges:
+        for x,y in edges:
             union(x,y)
-
+        
         return len(set([find(item) for item in range(len(isConnected))]))
+            
