62d17a33d9c7ad5b8d37f87d2e1fc7ab0cf2febc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-10 16:03:09 -0800

    Time: 3 ms (88.46%) | Memory: 17.9 MB (87.86%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,24 +1,19 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         from collections import defaultdict
-        graph, in_degree = defaultdict(list), [0]*numCourses
-
-        for i in range(0,len(prerequisites)):
-            dest, prev = prerequisites[i]
-            graph[prev].append(dest)
-            in_degree[dest] += 1
-
-        queue = [item for item in range(numCourses) if in_degree[item] == 0]
-        answer = []
+        in_edges,graph = [0]*numCourses, defaultdict(list)
+        for dest, previous in prerequisites:
+            graph[dest].append(previous)
+            in_edges[previous] +=1 
+        queue = [item for item in range(numCourses) if in_edges[item] == 0]
+        ans = []
         while queue:
             cur = queue.pop(0)
-            if [cur] in answer:
-                return False 
-            answer += [cur]
-            candidate = graph[cur]
-            for i in range(0,len(candidate)):
-                in_degree[candidate[i]] -=1 
-                if in_degree[candidate[i]] == 0:
-                    queue.append(candidate[i])
-        return len(answer) == numCourses
+            ans.append(cur)
+            cand = graph[cur]
+            for i in range(0,len(cand)):
+                in_edges[cand[i]] -=1 
+                if in_edges[cand[i]] == 0:
+                    queue.append(cand[i])
+        return len(ans) == numCourses
 
