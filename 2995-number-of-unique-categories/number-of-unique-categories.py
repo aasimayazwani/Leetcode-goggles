fdfe7ddee0c290c4b863cd880bea25c8a57f52fe
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-15 21:02:50 -0800

    Time: 1853 ms (31.58%) | Memory: 17.5 MB (9.88%) - LeetSync

--- 2995-number-of-unique-categories/number-of-unique-categories.py
+++ 2995-number-of-unique-categories/number-of-unique-categories.py
@@ -4,27 +4,27 @@
 #         pass
 class Solution:
     def numberOfCategories(self, n: int, categoryHandler: Optional['CategoryHandler']) -> int:
+        graph = {}
         edges = []
         for i in range(n):
-            for j in range(i,n):
+            for j in range(i+1,n):
                 if categoryHandler.haveSameCategory(i,j):
                     edges.append([i,j])
-        parent = {}
+        parents = {}
         for i in range(n):
-            parent[i] = i 
-    
-        def find(node):
-            if parent[node] != node:
-                parent[node] = find(parent[node])
-            return parent[node]
+            parents[i] = i
 
-        def union(nodex,nodey):
-            nodex, nodey = find(nodex), find(nodey)
-            if nodex != nodey:
-                parent[nodey] = nodex
-        
-        for x,y in edges:
+        def find(node):
+            if parents[node] != node:
+                parents[node] = find(parents[node])
+            return parents[node]
+
+        def union(nodex, nodey):
+            rootx, rooty = find(nodex), find(nodey)
+            if rootx != rooty:
+                parents[rooty] = rootx
+
+        for x, y in edges:
             union(x,y)
 
         return len(set([find(item) for item in range(n)]))
-        
