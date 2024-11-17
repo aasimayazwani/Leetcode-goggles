e617b1bf4c6606b014905004384d0a78e4216a60
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-16 23:26:32 -0800

    Time: 5 ms (6.90%) | Memory: 19.4 MB (11.89%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -6,23 +6,18 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        values = []
+        ans = []
+        
+
         def dfs(root):
             if root == None:
                 return 
-            else:
-                dfs(root.left)
-                values.append(root.val)
-                dfs(root.right)
-
-            return values
-
+            dfs(root.left)
+            ans.append((abs(target-root.val),root.val))
+            dfs(root.right)
+        
         dfs(root)
-        closest_val = values[0]
-        min_diff = abs(target - values[0])
-        for val in values[1:]:
-            if abs(target - val) < min_diff:
-                min_diff = abs(target - val)
-                closest_val = val
-                
-        return closest_val
+        heapq.heapify(ans)
+        a, b = heapq.heappop(ans)
+        return b
+        
