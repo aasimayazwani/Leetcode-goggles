c897684d4be4a2d4d060ec05790a4b739f3d3a9e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-18 21:58:15 -0800

    Time: 8 ms (37.80%) | Memory: 18.2 MB (17.23%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,34 +1,26 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        
-        def find(node):
-            if parent[node] != node:
-                parent[node] = find(parent[node])
-            return parent[node]
-
-        def union(nodex,nodey):
-            rootx, rooty = find(nodex), find(nodey)
-            if rootx != rooty:
-                parent[rooty] = rootx
-
-        edges = []
-        parent = {}
-        n = len(isConnected)
-        for i in range(n):
-            parent[i] = i
-
+        edges= []
         for i in range(0,len(isConnected)):
-            for j in range(0,len(isConnected[i])):
+            for j in range(i+1,len(isConnected)):
                 if isConnected[i][j] == 1:
                     edges.append([i,j])
+        n = len(isConnected)
+        parent = {}
+        for i in range(0,n):
+            parent[i] = i
         
+        def find(root):
+            if parent[root] != root:
+                parent[root] = find(parent[root])
+            return parent[root]
+        
+        def union(nodex,nodey):
+            rootx, rooty= find(nodex), find(nodey)
+            if rootx != rooty:
+                parent[rooty] = rootx
+            
         for x, y in edges:
             union(x,y)
 
         return len(set([find(item) for item in range(n)]))
-
-        
-
-        
-
-        
