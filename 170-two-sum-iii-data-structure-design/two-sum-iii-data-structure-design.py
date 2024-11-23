e47db481fc5f18ff0afdc8b38f47ec7137c1d913
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-23 13:30:41 -0800

    Time: 428 ms (31.49%) | Memory: 21.2 MB (65.03%) - LeetSync

--- 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
+++ 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
@@ -1,23 +1,27 @@
 class TwoSum:
 
     def __init__(self):
-        self.mapping = {}
+        self.arr  = []
 
     def add(self, number: int) -> None:
-        if number in self.mapping:
-            self.mapping[number] += 1
-        if number not in self.mapping:
-            self.mapping[number] = 1
+        self.arr.append(number)
+        self.arr.sort()
 
     def find(self, value: int) -> bool:
-        keys = list(self.mapping.keys())
-        for i in range(0,len(keys)):
-            left = value - keys[i]
-            if (left != keys[i]) and (left in self.mapping):
+        left, right = 0, len(self.arr)-1
+        while left < right:
+            cur = self.arr[left] + self.arr[right]
+            if cur == value:
                 return True 
-            if (left == keys[i]) and (self.mapping[left] > 1):
-                return True
+            elif cur < value:
+                left +=1 
+            elif cur > value:
+                right -=1 
+            
         return False
+
+        
+
 
 # Your TwoSum object will be instantiated and called as such:
 # obj = TwoSum()
