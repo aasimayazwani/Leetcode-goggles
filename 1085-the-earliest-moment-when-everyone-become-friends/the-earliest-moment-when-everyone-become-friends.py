f5d25785034e9785b58a443db89f205faf24bcbe
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-11-27 08:57:25 -0800

    Time: 3 ms (80.34%) | Memory: 17.5 MB (10.49%) - LeetSync

--- 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.py
+++ 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.py
@@ -1,39 +1,37 @@
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
         logs = sorted(logs,key=lambda x:x[0])
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
+
+        def union(nodex,nodey):
+            rootx, rooty = find(nodex), find(nodey)
+            if rootx != rooty:
+                if rank[rootx] >= rank[rooty]:
+                    parent[rooty] = rootx
+                    rank[rootx] +=1
+                else:
+                    parent[rootx] = rooty
+                    rank[rooty] +=1 
+                return True
+            return False
+
         parent = {}
         for i in range(0,n):
             parent[i] = i
-        rank = [0]*(n)
 
-        visited_components = n
-
-        def find(root):
-            if parent[root] != root:
-                parent[root] = find(parent[root])
-            return parent[root]
-
-        def union(time,rootx,rooty):
-            nonlocal visited_components
-            rootx, rooty =find(rootx), find(rooty)
-            if rootx != rooty:
-                if rank[rootx] < rank[rooty]:
-                    parent[rootx] = rooty
-                
-                elif rank[rootx] > rank[rooty]:
-                    parent[rooty] = rootx
-                
-                elif rank[rootx] == rank[rooty]:
-                    parent[rooty] = rootx
-                    rank[rootx] +=1 
-                    
-                visited_components -=1 
-                
-                if visited_components == 1:
-                    return time
+        for i in range(0,n):
+            parent[i] = find(i)
+        
+        rank = [0]*n
         for time, x, y in logs:
-            result = union(time, x, y)
-            if result is not None:
-                return result
-        return -1
-                        
+            #print(n)
+            if union(x,y):
+                n -=1
+                if n == 1:
+                    return time
+        return -1 
+            
+
