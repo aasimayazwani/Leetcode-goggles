ed7fbd0f6bef5ae821f61401ae9a08a56cf8f4c6
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-18 09:12:16 -0800

    Time: 1380 ms (5.17%) | Memory: 0B (100.00%) - LeetSync

--- 1712-unique-orders-and-customers-per-month/unique-orders-and-customers-per-month.sql
+++ 1712-unique-orders-and-customers-per-month/unique-orders-and-customers-per-month.sql
@@ -1,9 +1,7 @@
-select month, 
-count(order_id) as "order_count", 
-count(distinct customer_id) as "customer_count"
-from 
-(select order_id, customer_id, date_format(order_date,"%Y-%m") as 'month',
-invoice 
-from orders 
-where invoice > 20 ) as t1 
-group by month 
+# Write your MySQL query statement below
+select month, count(distinct order_id) as "order_count", count(distinct customer_id) as "customer_count"
+from
+(select date_format(order_date,"%Y-%m") as "month",  customer_id, order_date, order_id 
+    from orders
+    where invoice > 20)  as t1 
+group by month; 
