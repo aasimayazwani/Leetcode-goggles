5a0246ad735302bb25990b42e5570d11c89e9f2f
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-18 23:24:07 -0800

    Time: 848 ms (14.86%) | Memory: 0B (100.00%) - LeetSync

--- 2453-all-the-matches-of-the-league/all-the-matches-of-the-league.sql
+++ 2453-all-the-matches-of-the-league/all-the-matches-of-the-league.sql
@@ -1,5 +1,4 @@
-select  
-t1.team_name as "home_team",
-t2.team_name as "away_team"
+select 
+t1.team_name as "home_team", t2.team_name as "away_team"
 from teams as t1, teams as t2 
 where t1.team_name != t2.team_name ; 
