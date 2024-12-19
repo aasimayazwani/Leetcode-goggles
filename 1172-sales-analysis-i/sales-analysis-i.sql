89a960070493647d00971d032fa25243e0ef1db7
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-18 21:51:10 -0800

    Time: 1912 ms (8.68%) | Memory: 0B (100.00%) - LeetSync

--- 1172-sales-analysis-i/sales-analysis-i.sql
+++ 1172-sales-analysis-i/sales-analysis-i.sql
@@ -1,7 +1,11 @@
+# Write your MySQL query statement below
+with cte as
+(select seller_id, sum(price) as "total"
+from sales
+group by seller_id )
+
 select seller_id from
-    (select seller_id, dense_rank() over (order by total desc) as "ranking"
-    from
-    (select seller_id, sum(price) as "total"
-    from sales
-    group by seller_id) as t1) as t2 
-    where ranking = 1  ; 
+    (select seller_id, total, 
+    dense_rank() over (order by total desc ) as "ranking"
+    from cte ) as t1 
+where ranking = 1 ; 
