a5d933bd49175dc59b71352d2e2023dcf8262655
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-19 22:55:27 -0800

    Time: 1000 ms (13.89%) | Memory: 0B (100.00%) - LeetSync

--- 1180-game-play-analysis-ii/game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/game-play-analysis-ii.sql
@@ -1,10 +1,9 @@
-with cte as
-(select *, dense_rank() over (partition by player_id order by smallest asc) as "ranking"
+# Write your MySQL query statement below
+select player_id, device_id
 from
-(select player_id, device_id, min(event_date) as "smallest"
-from activity
-group by player_id, device_id) as t1 
-)
-
-select player_id, device_id from cte 
+(select player_id, 
+    device_id,
+    dense_rank() over (partition by player_id order by event_date asc) as "ranking"
+    from activity
+) as t1 
 where ranking = 1 ; 
