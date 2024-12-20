13927576ed75d40bdd2b9d68a80a83ee69651e31
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-19 22:46:45 -0800

    Time: 683 ms (19.79%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,4 +1,10 @@
-select customer_number from 
-(select customer_number, count(order_number) over (partition by customer_number) as "total" 
-from orders 
-order by total desc limit 1) as t1 
+# Write your MySQL query statement below
+with cte as 
+(select customer_number, count(order_number) as "counting"
+from orders
+group by customer_number)
+
+select customer_number from
+(select *, dense_rank() over (order by counting desc) as "ranking"
+from cte )  as t1 
+where ranking = 1 ; 
