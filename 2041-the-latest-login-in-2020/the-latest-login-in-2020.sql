1d0da42718d840b950e35ded6468064b69d88490
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-20 22:22:32 -0800

    Time: 565 ms (97.47%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-select user_id, max(time_stamp) as "last_stamp" from 
-logins 
-where year(time_stamp) = 2020
-group by user_id
+select user_id, max(time_stamp) as "last_stamp"
+from
+(select user_id, year(time_stamp) as "year", time_stamp
+from logins) as t1 
+where year = 2020 
+group by user_id ; ;
