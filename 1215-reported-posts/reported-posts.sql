d782f298b6e694aa0d3a4ce1ec67dabf90d884cc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-20 22:14:58 -0800

    Time: 913 ms (12.03%) | Memory: 0B (100.00%) - LeetSync

--- 1215-reported-posts/reported-posts.sql
+++ 1215-reported-posts/reported-posts.sql
@@ -1,8 +1,7 @@
-with cte as
-(select * from actions 
-where action_date = "2019-07-04" and 
-action = "report" and extra is not null)
-
-select extra as "report_reason",count(distinct post_id) as "report_count"
-from cte 
-group by extra 
+select  
+extra as 'report_reason', count(distinct post_id) as "report_count"
+from actions
+where 
+action_date = "2019-07-04" and 
+action = "report" 
+group by extra ; 
