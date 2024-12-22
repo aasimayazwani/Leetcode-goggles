522884d38f59d30c2b3cc865a9fa09c0b4c91498
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-21 17:21:43 -0800

    Time: 384 ms (80.90%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -1,5 +1,19 @@
-# Write your MySQL query statement belows
-select distinct t1.seat_id from cinema as t1, cinema as t2 
-where abs(t2.seat_id - t1.seat_id) = 1 and 
-t1.free = 1 and t2.free = 1
-order by seat_id asc  ; 
+# Write your MySQL query statement below
+with cte as
+(select 
+t1.seat_id as "seat1", t2.seat_id as "seat2"
+from 
+cinema as t1 ,
+cinema as t2 
+where 
+t2.seat_id - t1.seat_id = 1  and 
+t1.free = 1 and 
+t2.free = 1
+order by t1.seat_id asc) 
+
+select seat_id 
+from
+(select seat1 as "seat_id" from cte
+UNION 
+select seat2 as "seat_id" from cte) as t3 
+order by seat_id asc ; 
