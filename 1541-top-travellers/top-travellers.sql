81fa862d5ec292d684edf16b840aed4b94247459
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-27 15:17:28 -0800

    Time: 1864 ms (6.89%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -1,11 +1,11 @@
-with cte as 
-(select user_id, sum(distance) as "travelled_distance"
-from rides
-group by user_id )
-
-
-select t1.name, 
-ifnull(t2.travelled_distance,0) as "travelled_distance" from users as t1 
-left join cte as t2 
-on t1.id = t2.user_id 
+select name, travelled_distance from
+(select 
+t1.id,
+t1.name, ifnull(sum(ifnull(t2.distance,0)),0) as "travelled_distance"
+from users as t1 
+left join 
+rides as t2 
+on 
+t1.id = t2.user_id 
+group by t1.id ) as t3 
 order by travelled_distance desc, name asc ; 
