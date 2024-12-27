fb0b7fb3507a2f36148c9a7876ab5daec6b86f5f
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-27 09:36:07 -0800

    Time: 2331 ms (5.08%) | Memory: 0B (100.00%) - LeetSync

--- 1551-npv-queries/npv-queries.sql
+++ 1551-npv-queries/npv-queries.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-select t1.id, t1.year, ifnull(t2.npv,0) as "npv" 
+select 
+t1.id, t1.year, ifnull(t2.npv,0) as "npv"
 from queries as t1 
-left join
+left join 
 npv as t2 
-on t1.id = t2.id and 
-t1.year = t2.year ; 
+on
+t1.id = t2.id and t1.year = t2.year ; 
