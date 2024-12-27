5ff0de79ed1d8fbcad3ee91f8c7fa95deee6d466
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-27 10:01:44 -0800

    Time: 1097 ms (17.17%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,4 +1,4 @@
-select user_id, concat(upper(left(name,1)),lower(right(name,length(name)-1)))  as "name"
-
-from users 
+select user_id, 
+concat(upper(left(name,1)),lower(right(name,length(name)-1))) as "name"
+from users
 order by user_id 
