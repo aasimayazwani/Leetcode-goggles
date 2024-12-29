936011577500236ee5f2cc3773403e14f9b63417
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-29 15:26:31 -0800

    Time: 1161 ms (97.80%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,5 +1,11 @@
-select customer_id, count(*) as "count_no_trans"
-from
-(select customer_id from visits 
-where visit_id not in (select visit_id from transactions)) as t1 
+with cte as
+    (select visit_id, customer_id
+    from visits
+    where 
+    visit_id not in 
+    (select visit_id from transactions) ) 
+
+select customer_id, count(visit_id) as "count_no_trans"
+from 
+cte 
 group by customer_id ; 
