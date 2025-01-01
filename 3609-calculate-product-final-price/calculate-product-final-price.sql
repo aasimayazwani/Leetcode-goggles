122cb96fc636e571517e9982492b00076d551bfc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-31 19:15:10 -0800

    Time: 1174 ms (9.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3609-calculate-product-final-price/calculate-product-final-price.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select 
+t1.product_id, 
+t1.price*(1-ifnull(t2.discount/100,0)) as "final_price",
+t1.category
+from products as t1 
+left join 
+discounts as t2 
+on 
+t1.category = t2.category 
+order by t1.product_id asc ; 
