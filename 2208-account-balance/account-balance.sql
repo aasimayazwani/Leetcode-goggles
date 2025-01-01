07363bf9bc6a9b21eb643df444aa970f3e3cc00d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-31 18:25:33 -0800

    Time: 1263 ms (7.28%) | Memory: 0B (100.00%) - LeetSync

--- 2208-account-balance/account-balance.sql
+++ 2208-account-balance/account-balance.sql
@@ -1,13 +1,13 @@
-select account_id, day, balance
-from 
-(select account_id, day, sum(amount) over (partition by account_id order by day asc) as "balance"
-from 
-(select   
-account_id, day, 
+with cte as
+(select account_id, day, 
 case 
     when type = "Deposit" then amount
     when type = "Withdraw" then -amount
-end
-as "amount"
-from transactions) as t1 ) as t2 
-order by account_id asc , day asc ;
+end as "amount"
+from transactions )
+
+select account_id, day, sum(amount) over (partition by account_id order by day asc)
+as "balance"
+from
+cte 
+order by account_id asc, day asc ; 
