672ce949520a597018ed5b09d4b85409b0bcb78e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-01 08:13:03 -0800

    Time: 692 ms (6.40%) | Memory: 0B (100.00%) - LeetSync

--- 3286-class-performance/class-performance.sql
+++ 3286-class-performance/class-performance.sql
@@ -1,14 +1,4 @@
-# Write your MySQL query statement below
-
-with cte as
-(select *, dense_rank() over (order by total desc) as "highest"
-        , dense_rank() over (order by total asc) as "lowest"
+select max(total)-min(total) as "difference_in_score"
 from
-(select student_id, assignment1 + assignment2 + assignment3
-as "total"
-from scores) as t3)
-
-
-select distinct difference_in_score from 
-(select total - (select total from cte where lowest = 1) as "difference_in_score"
-from cte where highest = 1 ) as t6 ;
+    (select assignment1+assignment2+assignment3 as "total"
+    from scores) as t1 
