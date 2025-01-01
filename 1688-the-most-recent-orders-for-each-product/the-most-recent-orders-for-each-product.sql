0694002b14d417d23a948f382e88c65c826c6b35
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2024-12-31 18:34:35 -0800

    Time: 3487 ms (23.26%) | Memory: 0B (100.00%) - LeetSync

--- 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
+++ 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
@@ -1,17 +1,20 @@
-with cte as 
-(select distinct product_id, order_id, order_date
-from
-(select product_id, order_id, order_date, 
-    dense_rank() over (partition by product_id order by order_date desc) as "ranking"
-    from orders) as t1 
-    where ranking = 1)
+with cte as
+    (select order_id, product_id, order_date
+        from
+        (select *, dense_rank() over (partition by product_id order by order_date desc)
+        as "ranking"
+        from orders) as t1 
+        where ranking = 1 )
 
-select t2.product_name, 
-        t1.product_id, 
-        t1.order_id,
-        t1.order_date 
- from cte as t1 
+select 
+t3.product_name, 
+t3.product_id, 
+t2.order_id, 
+t2.order_date
+from cte as t2 
 inner join 
-products as t2 
-on t1.product_id = t2.product_id 
-order by product_name asc, product_id asc, order_id asc ; 
+products as t3 
+on 
+t2.product_id = t3.product_id 
+order by t3.product_name asc, 
+t3.product_id asc, t2.order_id asc ; 
