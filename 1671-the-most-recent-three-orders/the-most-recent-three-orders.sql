b81bfc40cb723cf0e4f82bb1cb3913a60ecca626
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-01 19:30:03 -0800

    Time: 1680 ms (21.69%) | Memory: 0B (100.00%) - LeetSync

--- 1671-the-most-recent-three-orders/the-most-recent-three-orders.sql
+++ 1671-the-most-recent-three-orders/the-most-recent-three-orders.sql
@@ -1,12 +1,18 @@
-with cte as 
-(select customer_id, order_id, order_date from 
-(select *, dense_rank() over (partition by customer_id order by order_date desc) 
-as "ranking" from orders ) as t1 
-where ranking <= 3 )
+with c1 as
+(select *, 
+dense_rank() over (partition by customer_id order by order_date desc) as "ranking"
+from orders
+)
 
-select t2.name as "customer_name",
-t3.customer_id, t3.order_id, t3.order_date from customers as t2 
+select 
+c0.name as "customer_name", 
+c1.customer_id, 
+c1.order_id, 
+c1.order_date 
+from customers as c0 
 inner join 
-cte as t3 
-on t2.customer_id = t3.customer_id 
-order by customer_name asc, customer_id asc, order_date desc
+c1 
+on 
+c0.customer_id = c1.customer_id 
+where ranking <= 3 
+order by customer_name asc, customer_id asc, order_date desc ; 
