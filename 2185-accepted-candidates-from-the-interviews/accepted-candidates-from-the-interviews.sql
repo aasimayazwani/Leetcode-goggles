a833725d0b47cf92b618b50c917e037056f6113c
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-01 17:48:21 -0800

    Time: 1726 ms (31.42%) | Memory: 0B (100.00%) - LeetSync

--- 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
+++ 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
@@ -1,11 +1,15 @@
-select candidate_id from
-(select t1.candidate_id, sum(t2.score)  as "total" 
-from 
-(select candidate_id, interview_id from candidates 
-where years_of_exp >= 2 ) as t1 
-inner join 
-rounds as t2 
-on t1.interview_id = t2.interview_id
-group by t1.candidate_id) as t3 
-where 
-total > 15 ; 
+with cte as
+    (select  
+    t1.candidate_id, t1.years_of_exp, t1.interview_id, t2.score
+    from candidates as t1 
+    inner join 
+    rounds as t2 
+    on 
+    t1.interview_id = t2.interview_id )
+
+select candidate_id
+from
+    (select candidate_id, sum(score) as "total", years_of_exp
+    from cte 
+    group by candidate_id) as t4 
+where years_of_exp >= 2 and total > 15 ; 
