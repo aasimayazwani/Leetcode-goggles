11cab2f4100516f664b540cdc2c82f9ac1cc1811
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-03 09:20:20 -0800

    Time: 1443 ms (27.80%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -2,27 +2,31 @@
 with cte as
 (select 
 t2.customer_id, 
-t1.price*t2.quantity as "total",
-date_format(t2.order_date,"%Y-%m") as "date"
+date_format(order_date,"%Y-%m") as "mon_val",
+t1.price*t2.quantity as "total"
 from product as t1 
-inner join 
+inner join
 orders as t2 
 on 
-t1.product_id = t2.product_id),
-
+t1.product_id = t2.product_id 
+),
 cte2 as
-(select customer_id, date,sum(total) as "total" 
-from cte 
-group by customer_id, date ), 
+(select customer_id, mon_val, sum(total)  as "total"
+from cte
+group by customer_id, mon_val),
 
 cte3 as
-(select distinct p1.customer_id  from cte2 as p1, cte2 as p2 
-where p1.customer_id = p2.customer_id and 
-p1.date = "2020-06" and p2.date = "2020-07" and 
-p1.total >= 100 and p2.total >= 100)
+(select p1.customer_id from cte2 as p1, cte2 as p2 
+where 
+p1.customer_id = p2.customer_id and 
+p1.mon_val = "2020-06" and 
+p2.mon_val = "2020-07" and
+p1.total >= 100 and 
+p2.total >= 100)
 
-select r1.customer_id, r1.name  from customers as r1
+select q1.customer_id, q2.name from 
+cte3 as q1 
 inner join 
-cte3 as r2 
+customers as q2 
 on 
-r1.customer_id = r2.customer_id 
+q1.customer_id = q2.customer_id 
