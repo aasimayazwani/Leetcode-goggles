fa7fe664629825070c8a0ac8f5e2ba1cc297cfdf
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-03 07:10:27 -0800

    Time: 2704 ms (5.12%) | Memory: 0B (100.00%) - LeetSync

--- 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
+++ 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
@@ -1,6 +1,14 @@
-select distinct t2.title from tvprogram as t1 
-inner join content as t2 
+# Write your MySQL query statement below
+with cte as
+(select date_format(program_date,"%Y-%m") as "val", content_id 
+from tvprogram)
+
+select distinct title from cte as t1 
+left join 
+content as t2 
 on 
 t1.content_id = t2.content_id 
-where t2.Kids_content = "Y" and t2.content_type = "Movies" and 
-date_format(t1.program_date, "%Y-%m") = "2020-06" ;
+where 
+t2.kids_content = "Y" and 
+t2.content_type ="Movies" and 
+t1.val ="2020-06"; 
