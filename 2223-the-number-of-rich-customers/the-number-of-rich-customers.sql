db895cf08f5c52d906bfb36311496c5690d9446d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-02 23:07:15 -0800

    Time: 401 ms (92.28%) | Memory: 0B (100.00%) - LeetSync

--- 2223-the-number-of-rich-customers/the-number-of-rich-customers.sql
+++ 2223-the-number-of-rich-customers/the-number-of-rich-customers.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
-select count(distinct customer_id) as "rich_count" from store 
-where amount > 500 ; 
+select count(distinct customer_id) as "rich_count"
+from store 
+where amount > 500 
