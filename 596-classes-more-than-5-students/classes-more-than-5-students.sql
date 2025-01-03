1e552be5f208a559be9b225c02a00cecef2c8015
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-02 21:18:10 -0800

    Time: 777 ms (8.49%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,9 +1,8 @@
 # Write your MySQL query statement belowselect 
 # class > count of students > filter for >=5 
-select class 
+select class
     from
-        (select class, count(distinct student) as "counting"
-        from courses
-        group by class )
-            as t1 
+    (select class, count(distinct student) as "counting"
+    from courses
+    group by class) as t1 
     where counting >= 5 ; 
