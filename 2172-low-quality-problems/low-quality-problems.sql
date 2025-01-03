4ffd948a0fbcf372cd68d96c429e72affef79c1e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-02 20:58:27 -0800

    Time: 446 ms (97.74%) | Memory: 0B (100.00%) - LeetSync

--- 2172-low-quality-problems/low-quality-problems.sql
+++ 2172-low-quality-problems/low-quality-problems.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-select problem_id from 
-problems 
-where (likes)/(likes+dislikes) < 0.6 
-order by problem_id asc ;
+select problem_id
+    from        
+    (select problem_id, likes/(likes+dislikes) as "ranking"
+    from 
+    problems) as t1 
+    where ranking < 0.6 
+    order by problem_id asc ; 
