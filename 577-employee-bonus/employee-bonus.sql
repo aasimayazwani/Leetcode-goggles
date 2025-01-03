2455e508044bb08a3f08ab041275b9545184cbfd
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-02 22:42:12 -0800

    Time: 1427 ms (30.41%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,4 +1,9 @@
-select  t1.name, t2.bonus from employee as t1 
-left join bonus as t2 
-on t1.empid = t2.empid 
-where t2.bonus < 1000  or t2.bonus is null ; 
+# Write your MySQL query statement below
+select 
+t1.name, ifnull(t2.bonus,null) as "bonus" 
+from employee as t1 
+left join 
+bonus as t2 
+on 
+t1.empid = t2.empid 
+where bonus is null or bonus < 1000; 
