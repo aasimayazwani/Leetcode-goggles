d1bdae2d8e3a4a778a840a88eb3b301b46539c59
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-02 21:38:25 -0800

    Time: 1429 ms (15.59%) | Memory: 0B (100.00%) - LeetSync

--- 2829-find-latest-salaries/find-latest-salaries.sql
+++ 2829-find-latest-salaries/find-latest-salaries.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-select emp_id, firstname,lastname, max(salary) as "salary", 
-department_id from salary 
-group by emp_id
+select 
+emp_id, firstname, lastname, max(salary) as "salary", 
+department_id 
+from
+salary 
+group by emp_id 
 order by emp_id asc ; 
