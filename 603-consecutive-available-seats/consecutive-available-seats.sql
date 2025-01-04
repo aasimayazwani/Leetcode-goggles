7e40d402b208eed48be78b97ae28183a7bb92402
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-03 16:48:27 -0800

    Time: 603 ms (28.98%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -1,19 +1,18 @@
 # Write your MySQL query statement below
-with cte as
+with cte 
+as
 (select 
-t1.seat_id as "seat1", t2.seat_id as "seat2"
-from 
-cinema as t1 ,
-cinema as t2 
+t1.seat_id as "seat1",t2.seat_id as "seat2"
+from cinema as t1, cinema as t2 
 where 
-t2.seat_id - t1.seat_id = 1  and 
+t2.seat_id - t1.seat_id = 1 and 
 t1.free = 1 and 
-t2.free = 1
-order by t1.seat_id asc) 
+t2.free = 1) 
 
-select seat_id 
+select distinct seat_id
 from
-(select seat1 as "seat_id" from cte
-UNION 
-select seat2 as "seat_id" from cte) as t3 
+    (select seat1 as "seat_id" from cte
+    union all
+    select seat2 as "seat_id" from cte)
+    as t1 
 order by seat_id asc ; 
