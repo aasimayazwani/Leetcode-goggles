85e5b77c4f34eebee5aeb3fea8764347c98384c5
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-05 14:55:20 -0800

    Time: 874 ms (32.47%) | Memory: 0B (100.00%) - LeetSync

--- 2446-the-first-day-of-the-maximum-recorded-degree-in-each-city/the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
+++ 2446-the-first-day-of-the-maximum-recorded-degree-in-each-city/the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
@@ -1,7 +1,8 @@
 select city_id, day, degree
     from
-    (select 
-    city_id, day, degree, dense_rank() over (partition by city_id order by degree desc,day asc) as "ranking"
-    from weather ) as t1 
+    (select *,
+            dense_rank() over (partition by city_id order by degree desc, day asc )
+            as "ranking"
+            from weather) as t1 
     where ranking = 1 
-order by city_id asc 
+    order by city_id asc ; 
