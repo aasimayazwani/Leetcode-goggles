8ebe3cd07472de80c4085923b05acbca6f8271f2
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-04 18:32:03 -0800

    Time: 1024 ms (20.00%) | Memory: 0B (100.00%) - LeetSync

--- 3281-find-peak-calling-hours-for-each-city/find-peak-calling-hours-for-each-city.sql
+++ 3281-find-peak-calling-hours-for-each-city/find-peak-calling-hours-for-each-city.sql
@@ -1,14 +1,17 @@
 # Write your MySQL query statement below
-
 with cte as
-(select city, peak_hour,count(*) as 'total' from
-(select *, hour(call_time) as "peak_hour"
-from calls) as t1 
-group by city, peak_hour)
+    (select city, peak_calling_hour, count(*) as "number"
+        from
+        (select hour(call_time) as "peak_calling_hour",
+                city
+                from calls) as t1 
+        group by city, peak_calling_hour)
 
-select city, peak_hour as "peak_calling_hour",
-total as number_of_calls from 
-(select *, dense_rank() over (partition by city order by total desc) as "ranking"
-from cte ) as t2 
-where ranking = 1 
-order by peak_calling_hour desc, city desc ;
+select city, peak_calling_hour, number as "number_of_calls"
+    from
+    (select city, peak_calling_hour, number,
+            dense_rank() over (partition by city order by number desc)
+            as "ranking"
+            from cte ) as t2 
+    where ranking = 1 
+    order by peak_calling_hour desc, city desc ; 
