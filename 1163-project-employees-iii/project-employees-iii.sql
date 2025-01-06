266982d146cffa2f4f92ece3e5344a6d5dcf1d6c
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-05 17:59:06 -0800

    Time: 1343 ms (10.46%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,10 +1,16 @@
-with cte as 
-(select project_id, employee_id, experience_years from
-(select t1.project_id, t1.employee_id, t2.experience_years from project as t1 
-inner join employee as t2 
-on t1.employee_id = t2.employee_id ) as t1 )
+with cte as
+    (select
+    t1.project_id, t2.employee_id, 
+    t2.experience_years
+    from project as t1 
+    inner join 
+    employee as t2 
+    on 
+    t1.employee_id = t2.employee_id) 
 
-select project_id, employee_id from 
-(select *, dense_rank() over (partition by project_id order by experience_years desc) as "ranking"
-from cte ) as t5 
-where ranking = 1 
+select project_id, employee_id
+    from
+    (select *, dense_rank() over (partition by project_id order by experience_years desc)
+        as "ranking"
+        from cte) as p1 
+    where ranking = 1 
