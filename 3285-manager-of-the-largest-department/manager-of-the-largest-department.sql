02e1cb989fe06c1809dd5b24cb7d6f5a6d7eff71
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-05 23:05:54 -0800

    Time: 397 ms (52.35%) | Memory: 0B (100.00%) - LeetSync

--- 3285-manager-of-the-largest-department/manager-of-the-largest-department.sql
+++ 3285-manager-of-the-largest-department/manager-of-the-largest-department.sql
@@ -1,16 +1,21 @@
-# Write your MySQL query statement below
-with cte as 
-(select dep_id, count(distinct emp_id) as 'counting'
-from employees
-group by dep_id)
+with cte as
+    (select *
+        from
+        (select
+            *, 
+            dense_rank() over (order by counting desc) as "ranking"
+            from
+                (select 
+                dep_id, count(distinct emp_id) as "counting"
+                from employees
+                group by dep_id) as t1) as t2 
+        where ranking = 1 )
 
-select employees.emp_name as "manager_name", employees.dep_id from employees 
-inner join 
-(select dep_id from 
-(select *, dense_rank() over (order by counting desc ) as "ranking"
-from cte )
-as t3 
-where ranking = 1 ) as t5 
-on t5.dep_id = employees.dep_id
-where  employees.position = "Manager"
-order by dep_id asc ;
+select 
+p2.emp_name as "manager_name", p1.dep_id 
+from cte as p1 
+inner join employees as p2
+on 
+p1.dep_id = p2.dep_id 
+where p2.position = "Manager" 
+order by dep_id asc ; 
