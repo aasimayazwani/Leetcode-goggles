67f353d558e7104dc83fcad953e2a36621ebdf54
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-05 20:53:42 -0800

    Time: 1065 ms (9.33%) | Memory: 0B (100.00%) - LeetSync

--- 2441-arrange-table-by-gender/arrange-table-by-gender.sql
+++ 2441-arrange-table-by-gender/arrange-table-by-gender.sql
@@ -1,9 +1,35 @@
-with cte as
-(select *, dense_rank() over (partition by gender order by user_id asc) as "ranking"
-from genders)
+with cte1 as
+(select *, 
+    row_number() over (order by user_id) as "ranking",
+    0 as "actual"
+    from
+    (select * from genders 
+    where gender = "female"
+    order by user_id asc) as t1 ),
 
-select  user_id, gender from 
-(select *, 1 as "position" from cte where gender  = "female" UNION ALL
-select *, 3 as "position" from cte where gender  = "male" UNION ALL
-select *, 2 as "position" from cte where gender  = "other") as t1 
-order by ranking asc, position asc ; 
+cte2 as
+(select *, 
+    row_number() over (order by user_id) as "ranking",
+    1 as "actual"
+    from
+    (select * from genders 
+    where gender = "other"
+    order by user_id asc) as t1 ),
+
+cte3 as
+(select *, 
+    row_number() over (order by user_id) as "ranking",
+    2 as "actual"
+    from
+    (select * from genders 
+    where gender = "male"
+    order by user_id asc) as t1 )
+
+select user_id, gender
+    from
+    (select * from cte1
+    union all
+    select * from cte2
+    union all
+    select * from cte3) as p1 
+order by ranking asc, actual asc ; 
