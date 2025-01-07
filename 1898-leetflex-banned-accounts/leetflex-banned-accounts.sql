ac76878e6974538a44eb3abb143cc1e21f84d4b1
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-06 22:07:26 -0800

    Time: 1702 ms (5.08%) | Memory: 0B (100.00%) - LeetSync

--- 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
-select distinct t1.account_id  from loginfo as t1, 
-loginfo as t2 
-where t1.account_id = t2.account_id and 
+select 
+distinct t1.account_id from loginfo as t1, loginfo as t2
+where 
+t1.account_id = t2.account_id and 
 t1.ip_address != t2.ip_address and 
-t1.login <= t2.logout and t2.login <= t1.logout  ; 
+((t1.login between t2.login and t2.logout) or
+(t2.login between t1.login and t1.logout) )
