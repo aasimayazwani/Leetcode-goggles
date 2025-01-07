32e6a98183611b8034d536c461468302ef0bcd40
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-07 08:51:45 -0800

    Time: 1639 ms (9.43%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,13 +1,12 @@
-# Write your MySQL query statement below
 with cte as
-(select activity,count(name) as "total" 
-from friends
-group by activity)
+    (select *, dense_rank() over (order by counting desc) as "forward"
+            , dense_rank() over (order by counting asc) as "backward"
+        from
+        (select activity, count(*) as "counting"
+        from friends
+        group by activity) as t1)
 
-
-select distinct activity from cte 
-where total not in (
-    select min(total) from  cte 
-    UNION ALL
-    select max(total) from  cte 
-)
+select distinct activity from friends
+where 
+activity not in (select distinct activity from cte 
+where forward =1 or backward = 1)  ;
