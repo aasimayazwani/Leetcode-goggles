6d0b00ff219f89be81f187ab7fd0c20b5e393c88
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-07 06:50:21 -0800

    Time: 1285 ms (9.50%) | Memory: 0B (100.00%) - LeetSync

--- 1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -1,12 +1,20 @@
-with cte as
-(select user_id, visit_date, 
-        lead(visit_date,1,"2021-1-1") over (partition by user_id order by visit_date)
-        as "next_day"
-        from uservisits)
+with cte as 
+    ((select distinct user_id, "2021-1-1" as "visit_date"
+    from UserVisits)
+    union all 
+    (select user_id, visit_date from uservisits)
+    ),
+
+cte2 as
+(select *, row_number() over (partition by user_id order by visit_date asc ) as "ranking"
+from cte )
 
 select user_id, max(biggest_window) as "biggest_window"
     from
-    (select user_id, abs(datediff(visit_date,next_day)) as "biggest_window" from cte )
-    as t1 
+    (select p1.user_id, datediff(p2.visit_date,p1.visit_date) as "biggest_window"
+    from cte2 as p1, cte2 as p2 
+    where
+    p1.user_id = p2.user_id and 
+    cast(p2.ranking as signed) - cast(p1.ranking as signed) = 1) as p3 
     group by user_id 
     order by user_id asc ; 
