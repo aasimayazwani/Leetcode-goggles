e38b4536d3d909806a54fd7376e0d051be28089e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-08 16:51:18 -0800

    Time: 1509 ms (39.73%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,9 +1,7 @@
-select product_id, first_year, quantity, price 
-from
-(select product_id, year as "first_year", 
-        quantity, 
-        price, 
-        dense_rank() over (partition by product_id order by year asc) as "ranking"
-        from sales 
-) as t1 
-where ranking = 1 
+select
+    product_id, year as "first_year", quantity, price
+    from
+    (select *, 
+    dense_rank() over (partition by product_id order by year asc ) as "ranking"
+    from sales) as t1
+    where ranking = 1 
