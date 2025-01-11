7e75033d36f163c3d64a24157aa27bc5986a778e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-10 20:39:42 -0800

    Time: 563 ms (31.35%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,5 +1,10 @@
-select t1.sale_date , t1.sold_num - t2.sold_num as "diff" 
-from sales as t1, sales as t2 
-where t1.sale_date = t2.sale_date and 
-t1.fruit = "apples" and t2.fruit = "oranges" 
-order by sale_date asc  ;
+# Write your MySQL query statement below
+select 
+t1.sale_date, t1.sold_num - t2.sold_num as "diff"
+from 
+sales as t1, sales as t2 
+where 
+t1.sale_date = t2.sale_date and 
+t1.fruit = "apples" and 
+t2.fruit = "oranges" 
+order by sale_date asc ; 
