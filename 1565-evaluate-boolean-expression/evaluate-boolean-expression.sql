f4fefe932e01a99aefb97de5aaf4f713dc10548e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-11 12:15:10 -0800

    Time: 3145 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

--- 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -1,14 +1,30 @@
-SELECT e.left_operand, e.operator, e.right_operand,
-    (
-        CASE
-            WHEN e.operator = '<' AND v1.value < v2.value THEN 'true'
-            WHEN e.operator = '=' AND v1.value = v2.value THEN 'true'
-            WHEN e.operator = '>' AND v1.value > v2.value THEN 'true'
-            ELSE 'false'
-        END
-    ) AS value
-FROM Expressions e
-JOIN Variables v1
-ON e.left_operand = v1.name
-JOIN Variables v2
-ON e.right_operand = v2.name
+# Write your MySQL query statement below
+with cte as
+    (select 
+    t2.left_operand, 
+    t2.operator, 
+    t2.right_operand, 
+    t1.value as "left_val"
+    from variables as t1 
+    inner join 
+    expressions as t2 
+    on 
+    t1.name = t2.left_operand), 
+
+cte2 as
+    (select 
+    *, p1.value as "right_val" 
+    from variables as p1 
+    inner join 
+    cte as p2 
+    on 
+    p1.name = p2.right_operand)
+
+select left_operand, operator, right_operand, 
+    case 
+        when operator = "=" and left_val = right_val then "true" 
+        when operator = ">" and left_val > right_val then "true" 
+        when operator = "<" and left_val < right_val then "true" 
+        else "false"
+    end as "value"
+from cte2 
