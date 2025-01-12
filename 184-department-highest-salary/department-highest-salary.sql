1cc73ec2d5335fc43de92ca2ebacef89fbbd2715
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-11 16:41:41 -0800

    Time: 1193 ms (32.03%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,18 +1,20 @@
+# Write your MySQL query statement below
 with cte as
-    (select departmentid, salary, name
+    (select name, departmentid, salary
         from
-        (select *, 
-            dense_rank() over (partition by departmentid order by salary desc)
+        (select *, dense_rank() over (partition by departmentid order by salary desc)
             as "ranking"
             from employee) as t1 
-        where ranking = 1 )
+        where 
+        ranking = 1)
 
-select 
+select
 p2.name as "Department",
 p1.name as "Employee",
 p1.salary as "Salary"
-from cte as p1 
+from 
+cte as p1 
 inner join 
-department as p2
+department as p2 
 on 
-p1.departmentid = p2.id
+p1.departmentid = p2.id ; 
