acf0aa57294ff00157d8ed0174605a851fc0ac6d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-12 14:48:35 -0800

    Time: 1097 ms (34.74%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,14 +1,23 @@
 # Write your MySQL query statement below
-with cte as
-    (select *
+with cte as 
+    (select 
+        delivery_id, 
+        customer_id, 
+        order_date, 
+        customer_pref_delivery_date
         from
         (select *, 
-            dense_rank() over (partition by customer_id order by order_date) as "ranking", 
-            case 
-                when order_date =customer_pref_delivery_date then 1 
-                else 0 
-                end as "status"
-            from delivery) as t1 
-        where ranking = 1)
+            dense_rank() over (partition by customer_id order by order_date)
+            as "ranking"
+            from delivery) as t1
+        where ranking =1),
 
-select round(100*(sum(status)/count(*)),2) as "immediate_percentage" from cte 
+cte2 as
+    (select *, 
+        case 
+            when order_date = customer_pref_delivery_date then 1 
+            else 0 
+        end as "value"
+        from cte)
+
+select round((sum(value)/count(*))*100,2) as "immediate_percentage"  from cte2 ; 
