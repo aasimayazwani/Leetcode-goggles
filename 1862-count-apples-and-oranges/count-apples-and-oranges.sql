dcac2d047b30946712afa369d4deb87c8df3ea6e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-11 17:15:37 -0800

    Time: 919 ms (92.07%) | Memory: 0B (100.00%) - LeetSync

--- 1862-count-apples-and-oranges/count-apples-and-oranges.sql
+++ 1862-count-apples-and-oranges/count-apples-and-oranges.sql
@@ -1,8 +1,10 @@
+# Write your MySQL query statement below
 select 
-sum(t1.apple_count + ifnull(t2.apple_count,0)) as "apple_count",
-sum(t1.orange_count + ifnull(t2.orange_count,0)) as "orange_count"
-from boxes as t1
+sum(ifnull(t1.apple_count,0)
++ifnull(t2.apple_count,0)) as "apple_count",
+sum(ifnull(t1.orange_count,0) +ifnull(t2.orange_count,0)) as "orange_count"
+from boxes as t1 
 left join 
 chests as t2 
 on 
-t1.chest_id = t2.chest_id ; 
+t1.chest_id = t2.chest_id
