e29b61d56b1590a95f5184e9adea8f8646575639
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-11 18:28:14 -0800

    Time: 788 ms (33.57%) | Memory: 0B (100.00%) - LeetSync

--- 574-winning-candidate/winning-candidate.sql
+++ 574-winning-candidate/winning-candidate.sql
@@ -1,13 +1,16 @@
+# Write your MySQL query statement below
 with cte as
-(select t1.name as "name",ifnull(t2.counting,0) as "counting" 
-from candidate as t1 
-left join
-(select candidateid as "candidateid", count(candidateid) as "counting"
-from vote
-group by candidateid) as t2 
-on t1.id = t2.candidateid) 
+    (select 
+    t1.name, count(t2.candidateid) as "counting"
+    from candidate as t1 
+    inner join 
+    vote as t2 
+    on 
+    t1.id = t2.candidateid 
+    group by t1.name)
 
-select name from
-(select *, dense_rank() over (order by counting desc, name desc ) as "ranking" from cte )
-as t3 
-where ranking = 1 
+select name
+    from
+    (select *, dense_rank() over (order by counting desc) as "ranking"
+    from cte) as t3 
+    where ranking = 1 ; 
