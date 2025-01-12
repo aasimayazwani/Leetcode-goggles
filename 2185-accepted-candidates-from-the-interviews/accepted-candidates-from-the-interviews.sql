9aec82256d3d8d609e092dcf0937ddbabb36148b
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-11 18:58:18 -0800

    Time: 2737 ms (9.48%) | Memory: 0B (100.00%) - LeetSync

--- 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
+++ 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
@@ -1,15 +1,19 @@
+# Write your MySQL query statement below
 with cte as
-    (select  
-    t1.candidate_id, t1.years_of_exp, t1.interview_id, t2.score
-    from candidates as t1 
-    inner join 
-    rounds as t2 
-    on 
-    t1.interview_id = t2.interview_id )
+    (select 
+    interview_id, sum(score) as "total"
+    from rounds
+    group by interview_id)
 
 select candidate_id
-from
-    (select candidate_id, sum(score) as "total", years_of_exp
-    from cte 
-    group by candidate_id) as t4 
-where years_of_exp >= 2 and total > 15 ; 
+    from
+    (select  
+    t1.candidate_id, t1.interview_id, t2.total, t1.years_of_exp
+    from 
+    candidates as t1 
+    inner join cte as t2 
+    on 
+    t1.interview_id = t2.interview_id) as t3 
+    where 
+    years_of_exp >=2 and 
+    total > 15 ; 
