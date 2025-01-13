9473ae8c16f9169faba092eec555f80915ec64bc
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-12 22:26:33 -0800

    Time: 559 ms (30.00%) | Memory: 0B (100.00%) - LeetSync

--- 3155-highest-salaries-difference/highest-salaries-difference.sql
+++ 3155-highest-salaries-difference/highest-salaries-difference.sql
@@ -1,11 +1,13 @@
 # Write your MySQL query statement below
-with cte as 
-(select * from
-(select *, dense_rank() over (partition by department order by salary desc) as "ranking"
-from salaries) as t1 
-where ranking = 1 )
+with cte as
+    (select department, salary
+        from
+        (select *, 
+            dense_rank() over (partition by department order by salary desc) as "ranking"
+            from salaries) as t1 
+        where ranking =1)
 
-select abs(t3.salary-t2.salary) as "salary_difference" from cte as t2 , 
-cte as t3 
-where t2.department = "Engineering" and 
-t3.department = "Marketing"
+select abs(t1.salary - t2.salary) as "salary_difference"
+from cte as t1, cte as t2 
+where 
+t1.department = "Marketing" and t2.department = "Engineering" ; 
