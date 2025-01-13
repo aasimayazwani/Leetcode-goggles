7fbd94932603b2246109741a0367ba7c991cbf9b
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-12 21:36:07 -0800

    Time: 801 ms (39.92%) | Memory: 0B (100.00%) - LeetSync

--- 3097-total-traveled-distance/total-traveled-distance.sql
+++ 3097-total-traveled-distance/total-traveled-distance.sql
@@ -1,8 +1,13 @@
+# Write your MySQL query statement below
+with cte as
+    (select user_id, sum(distance) as "traveled_distance"
+    from rides
+    group by user_id)
+
 select 
-t1.user_id, t1.name, ifnull(sum(t2.distance),0) as "traveled distance"
- from users as t1 
+t1.user_id, t1.name as "name",ifnull(t2.traveled_distance,0) as "traveled distance"
+from users as t1 
 left join 
-rides as t2 
-on t1.user_id = t2.user_id 
-group by t1.user_id 
+cte as t2 on 
+t1.user_id = t2.user_id 
 order by user_id asc ; 
