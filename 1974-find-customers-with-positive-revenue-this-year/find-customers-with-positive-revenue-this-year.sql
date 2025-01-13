31e2ac6d8464da9f903ba8317676c0d344780f84
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-12 21:15:06 -0800

    Time: 1235 ms (30.78%) | Memory: 0B (100.00%) - LeetSync

--- 1974-find-customers-with-positive-revenue-this-year/find-customers-with-positive-revenue-this-year.sql
+++ 1974-find-customers-with-positive-revenue-this-year/find-customers-with-positive-revenue-this-year.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
-select customer_id 
-    from 
-    (select customer_id, year, sum(revenue) as "total"
-    from Customers
-    group by customer_id, year)
-    as t1 
-where total > 0 and year = 2021 ; 
+select customer_id
+    from
+    (select customer_id, sum(revenue) as "total"
+    from customers 
+    where year = 2021 
+    group by customer_id) as t1 
+    where total > 0 ; 
