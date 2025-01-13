37fcfe533d8f1e516997d67c81428c34ead356d5
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-12 20:31:28 -0800

    Time: 566 ms (27.56%) | Memory: 0B (100.00%) - LeetSync

--- 3255-pizza-toppings-cost-analysis/pizza-toppings-cost-analysis.sql
+++ 3255-pizza-toppings-cost-analysis/pizza-toppings-cost-analysis.sql
@@ -1,18 +1,15 @@
-# Write your MySQL query statement below
 with cte as
-    (select *, row_number() over (order by topping_name asc ) as "ranking"
+    (select *, row_number() over (order by topping_name asc ) as "numbering"
         from
-        (select * from toppings order by topping_name asc) as t1)
+        (select * from toppings 
+        order by topping_name asc ) as t1 )
 
 select 
-concat(t1.topping_name,",",t2.topping_name,",",t3.topping_name) as "pizza", t1.cost+t2.cost+t3.cost as "total_cost"
-from 
-cte as t1, cte as t2, cte as t3
+concat(t1.topping_name,",",t2.topping_name,",",t3.topping_name) as "pizza", 
+t1.cost+t2.cost+t3.cost as "total_cost"
+from cte as t1, cte as t2, cte as t3
 where 
-t1.topping_name != t2.topping_name and 
-t2.topping_name != t3.topping_name and 
-t1.topping_name != t3.topping_name and
-t1.ranking < t2.ranking and 
-t2.ranking < t3.ranking and 
-t1.ranking < t3.ranking 
-order by total_cost desc, pizza asc ;
+t1.numbering < t2.numbering and 
+t2.numbering < t3.numbering and 
+t1.numbering < t3.numbering 
+order by total_cost desc, pizza asc ;  
