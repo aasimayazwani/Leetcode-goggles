d27886cbc58d168c0275c8b7edd3f482bfe6c886
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-15 22:22:15 -0800

    Time: 7 ms (4.16%) | Memory: 18.2 MB (5.33%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,30 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        left = ["[","(","{"]
-        right = ["]",")","}"]
-        s = [item for item in s]
-        mapping = {}
-        for i in range(0,len(left)):
-            mapping[left[i]] = right[i]
+        right = {")":"(","}":"{","]":"["}
         stack = []
+        s = [item for item in s]
         while len(s) > 0:
             if len(stack) == 0:
-                stack.append(s[0])
-                s.pop(0)
+                if s[0] in right:
+                    return False 
+                else:
+                    stack.append(s.pop(0))
             else:
-                if s[0] in mapping:
-                    stack.append(s[0])
-                    s.pop(0)
-                else :
-                    if stack[-1] in mapping:
-                        if mapping[stack[-1]] == s[0]:
-                            stack.pop(-1)
-                            s.pop(0)
-                        else:
-                            return False
+                if s[0] not in right:
+                    stack.append(s.pop(0))
+                else:
+                    if right[s[0]] == stack[-1]:
+                        stack.pop(-1)
+                        s.pop(0)
                     else:
-                        return False
-        if len(stack) == 0:
-            return True
-        return False
-                
+                        return False 
+        if len(s) == 0 and len(stack) == 0:
+            return True 
+        return False 
+
