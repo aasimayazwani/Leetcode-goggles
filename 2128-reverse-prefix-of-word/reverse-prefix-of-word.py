8dc4ec0037f46a60de4ec80d18d04ee86d1e926d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-01-15 23:17:50 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (21.03%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -1,12 +1,14 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         word = [item for item in word]
-        stack = []
         for i in range(0,len(word)):
             if word[i] == ch:
-                stack.append(ch)
-                return "".join(stack[::-1]) + "".join(word[i+1:])
-            elif word[i] != ch:
-                stack.append(word[i])
+                return self.reversing(word,0,i)
         return "".join(word)
-            
+    
+    def reversing(self,word,left,right):
+        while left < right:
+            word[left], word[right] = word[right], word[left]
+            left +=1 
+            right -=1 
+        return "".join(word)
