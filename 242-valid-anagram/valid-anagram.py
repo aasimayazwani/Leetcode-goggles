07b6c43f462149583767301ffdc142f62de12f67
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-02-03 21:33:23 -0800

    Time: 7 ms (90.18%) | Memory: 17.9 MB (57.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # the question is asking us to preserve the dictionary of s and t
         from collections import Counter
-        s_dict = Counter(s)
-        t_dict = Counter(t)
-        if s_dict == t_dict:
-            return True 
-        return False
+        return Counter(s) == Counter(t)
+    
