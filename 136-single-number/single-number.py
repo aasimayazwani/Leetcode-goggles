037d488b039c3b9361f38844778913edadc2b098
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-02-08 11:24:19 -0800

    Time: 7 ms (34.77%) | Memory: 20 MB (6.81%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -2,4 +2,4 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         from collections import Counter
         mapping = Counter(nums)
-        return [item[0] for item in mapping.items() if item[1] == 1 ][0]
+        return [key for (key,value) in mapping.items() if value == 1][0]
