7304dbcc9a95803b0d7b1aebdf035b2c5962fcc9
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-02-08 11:26:24 -0800

    Time: 1412 ms (85.81%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 select 
-ifnull(t2.unique_id,Null) as "unique_id", t1.name 
+ifnull(t2.unique_id,Null) as "unique_id", 
+t1.name
 from employees as t1 
 left join 
 employeeuni as t2 
 on 
-t1.id = t2.id 
+t1.id = t2.id ;
