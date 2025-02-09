e46aa34f6eab819865a5951adaf3eb95cf8dd76e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-02-09 12:19:49 -0800

    Time: 7 ms (49.77%) | Memory: 18.9 MB (79.30%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,20 +1,20 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        from collections import defaultdict 
-        graph, in_edges = defaultdict(list), [0]*numCourses
+        from collections import Counter, defaultdict
+        mapping = defaultdict(list)
+        indegree = [0]*numCourses
         for prev, dest in prerequisites:
-            in_edges[dest] +=1 
-            graph[prev].append(dest)
-        queue = [item for item in range(numCourses) if in_edges[item] == 0 ]
+            mapping[prev].append(dest)
+            indegree[dest] +=1 
+        queue = [i for i in range(0,numCourses) if indegree[i] == 0]
         visited = set()
         while queue:
             cur = queue.pop(0)
             visited.add(cur)
-            cand = graph[cur]
+            cand = mapping[cur]
             for i in range(0,len(cand)):
-                in_edges[cand[i]] -=1 
-
-                if in_edges[cand[i]] == 0:
+                indegree[cand[i]] -=1
+                if indegree[cand[i]] == 0:
                     queue.append(cand[i])
         return len(visited) == numCourses
         
