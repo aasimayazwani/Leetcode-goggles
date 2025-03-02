2cb9be3cdb32e100dcb3fcb808dcb589bfa5f32e
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-02 15:12:56 -0800

    Time: 775 ms (31.66%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -3,5 +3,5 @@ select actor_id, director_id
     from
     (select actor_id, director_id, count(*) as "counting"
     from actordirector
-    group by actor_id, director_id)  as  t1
-    where counting >= 3 ; 
+    group by actor_id, director_id) as t1 
+    where counting >= 3; 
