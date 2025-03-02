e37d06c861a96d0e856a1f9273bac8b7e1403351
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-02 14:30:58 -0800

    Time: 1500 ms (7.86%) | Memory: 0B (100.00%) - LeetSync

--- 2172-low-quality-problems/low-quality-problems.sql
+++ 2172-low-quality-problems/low-quality-problems.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-select problem_id
-    from        
-    (select problem_id, likes/(likes+dislikes) as "ranking"
-    from 
-    problems) as t1 
-    where ranking < 0.6 
+select distinct problem_id
+    from
+    (select problem_id, ((likes)/(likes+dislikes)) as "percent"
+    from problems) as t1 
+    where percent < 0.6 
     order by problem_id asc ; 
