47ebd372f506d379eae3bfc8529c4076f16660d4
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-02 16:42:09 -0800

    Time: 522 ms (53.27%) | Memory: 0B (100.00%) - LeetSync

--- 3554-premier-league-table-ranking/premier-league-table-ranking.sql
+++ 3554-premier-league-table-ranking/premier-league-table-ranking.sql
@@ -1,11 +1,6 @@
 # Write your MySQL query statement below
-with cte as
-(select 
-team_id, 
-team_name, 
-3*wins + draws as "points"
-from TeamStats )
-
 select *, rank() over (order by points desc) as "position"
-from cte
-order by points desc, team_name asc ; 
+    from
+    (select team_id, team_name, 3*wins + draws as "points"
+    from teamstats) as t1 
+order by points desc, team_name asc ;
