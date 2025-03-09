41ad92d8a4f22ce30332b1c740a2882d9de1c29f
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-08 23:25:19 -0800

    Time: 1097 ms (37.62%) | Memory: 0B (100.00%) - LeetSync

--- 2446-the-first-day-of-the-maximum-recorded-degree-in-each-city/the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
+++ 2446-the-first-day-of-the-maximum-recorded-degree-in-each-city/the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
@@ -1,8 +1,7 @@
+# Write your MySQL query statement below
 select city_id, day, degree
     from
-    (select *,
-            dense_rank() over (partition by city_id order by degree desc, day asc )
-            as "ranking"
-            from weather) as t1 
+    (select *, dense_rank() over (partition by city_id order by degree desc, day asc ) as "ranking"
+        from weather) as t1 
     where ranking = 1 
     order by city_id asc ; 
