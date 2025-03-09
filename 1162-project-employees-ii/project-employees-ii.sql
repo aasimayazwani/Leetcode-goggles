cea476949ea89a2e31e70211ee9d3258c1b0ea34
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-08 16:39:14 -0800

    Time: 1685 ms (31.36%) | Memory: 0B (100.00%) - LeetSync

--- 1162-project-employees-ii/project-employees-ii.sql
+++ 1162-project-employees-ii/project-employees-ii.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
-with cte as 
-(select project_id, dense_rank() over (order by total desc ) as "ranking" from 
-(select project_id, count(distinct employee_id) as "total" from project 
-group by project_id) as t1 )
+with cte as
+    (select project_id, count(employee_id) as "counting"
+    from project
+    group by project_id)
 
-select project_id from cte 
-where ranking = 1 ; 
+select project_id
+    from
+    (select *, dense_rank() over (order by counting desc) as "ranking"
+    from cte) as t1 
+    where 
+    ranking = 1 ; 
