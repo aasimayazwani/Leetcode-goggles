f59963541a542ccbff7ecb76d2ff6a119e0129bb
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-09 20:36:09 -0700

    Time: 1269 ms (29.12%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,15 +1,11 @@
-with cte as 
-(select * from
-    (select from_id as "person1", 
-            to_id as "person2", duration
-            from calls
-            UNION ALL 
-            select to_id as "person1", 
-                from_id  as "person2", duration
-                    from calls) 
-    as t1 
-    where person1  < person2 )
+# Write your MySQL query statement below
+with cte as
+    (select from_id as "person1", to_id  as "person2", duration from calls
+    union all 
+    select to_id  as "person1", from_id  as "person2", duration from calls)
 
-select person1, person2, count(*) as "call_count", sum(duration) as "total_duration"
+select person1, person2, count(*) as "call_count" , 
+sum(duration) as "total_duration"
 from cte
-group by person1, person2 
+where person1 < person2
+group by person1, person2
