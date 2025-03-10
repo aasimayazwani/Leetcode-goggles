48c0085a55cf2eefd8580b17d5b0f0964b4be56f
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-09 17:57:25 -0700

    Time: 1442 ms (19.62%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,16 +1,12 @@
+# Write your MySQL query statement below
 with cte as
-    (select
-    t1.project_id, t2.employee_id, 
-    t2.experience_years
-    from project as t1 
-    inner join 
-    employee as t2 
-    on 
-    t1.employee_id = t2.employee_id) 
+    (select 
+    t1.project_id, t2.employee_id, t2.experience_years
+    from project as t1 inner join 
+    employee as t2 on t1.employee_id = t2.employee_id )
 
-select project_id, employee_id
-    from
-    (select *, dense_rank() over (partition by project_id order by experience_years desc)
-        as "ranking"
-        from cte) as p1 
+select project_id, employee_id from
+    (select *, 
+        dense_rank() over (partition by project_id order by experience_years desc) as "ranking"
+        from cte) as t3 
     where ranking = 1 
