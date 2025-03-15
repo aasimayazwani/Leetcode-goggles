13d635aae79bc537e6f6624db891a4d0a7539f8d
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-15 15:14:08 -0700

    Time: 3371 ms (5.74%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,19 +1,13 @@
 # Write your MySQL query statement below
 with cte as
-    (select 
-    t2.product_id, t1.product_name, 
-    t2.sale_date 
-    from product as t1 
-    inner join 
-    sales as t2 
-    on 
-    t1.product_id = t2.product_id) 
+    (select product_id from sales 
+    where sale_date between "2019-01-01" and "2019-03-31" 
+    and product_id not in (
+        select product_id from sales 
+        where sale_date not between "2019-01-01" and "2019-03-31" 
+    ))
 
-select 
-    distinct product_id, product_name 
-    from 
-        cte 
-    where sale_date between "2019-01-01" and "2019-03-31"
-    and product_id not in 
-        (select product_id from cte 
-        where sale_date < "2019-01-01" or sale_date > "2019-03-31") ; 
+select distinct p1.product_id, p2.product_name from cte as p1 
+inner join 
+product as p2 
+on p1.product_id = p2.product_id 
