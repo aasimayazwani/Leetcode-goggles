64e0032f5b57395ed02e87116c27443f7f650fd7
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-14 23:40:10 -0700

    Time: 997 ms (62.11%) | Memory: 0B (100.00%) - LeetSync

--- 1453-ads-performance/ads-performance.sql
+++ 1453-ads-performance/ads-performance.sql
@@ -1,26 +1,16 @@
 # Write your MySQL query statement below
-with cte as 
-    (select
-        ad_id, user_id, 
-        case 
-            when action = "Clicked" then 1 
-            when action = "Viewed" then 0
-        end 
-        as "action"
-        from
-        (select ad_id, user_id, action 
-        from ads 
-        where action != "Ignored") as a1),
+with cte as
+    (select ad_id, 
+        case when action = "Clicked" then 1 
+        else 0 
+        end as "clicked",
+        case when action = "Clicked" then 1 
+        when action = "Viewed" then 1 
+        else 0 
+        end as "total"
+        from ads)
 
-
-results as
-(select ad_id, round(100*(sum(action)/count(action)),2) as "ctr"
+select ad_id, ifnull(round(100*(sum(Clicked)/sum(total)),2),0) as "ctr"
 from cte 
-group by ad_id)
-
-select distinct t1.ad_id, ifnull(t2.ctr,0) as "ctr"
-from ads as t1
-left join 
-results as t2
-on t1.ad_id = t2.ad_id
-order by  ctr desc, ad_id asc 
+group by ad_id 
+order by ctr desc, ad_id asc ; 
