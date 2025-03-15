3de9f6903ca5b34c228de8b70714af92f0a67358
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-15 06:22:38 -0700

    Time: 1535 ms (23.28%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,13 +1,14 @@
-with cte as 
-(select employee_id, reports_count, average_age
-    from
-    (select reports_to as "employee_id", count(distinct employee_id) as "reports_count", round(avg(age),0) as "average_age"
-    from employees
-    where reports_to is not null
-    group by reports_to) as t1 )
+# Write your MySQL query statement below
+with cte as
+    (select reports_to as "employee_id", 
+            count(*) as "reports_count",
+            round(avg(age)) as "average_age"
+            from employees
+            where reports_to is not null
+            group by reports_to
+            )
 
-select t1.employee_id, t2.name, t1.reports_count, t1.average_age from cte as t1 
-inner join 
-employees as t2 
-on t1.employee_id = t2.employee_id 
-order by t1.employee_id asc ; 
+select t1.employee_id, t2.name , t1.reports_count, t1.average_age from cte as t1 
+inner join employees as t2 
+on t1.employee_id = t2.employee_id
+order by employee_id asc ; 
