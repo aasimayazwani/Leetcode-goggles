1fe89b153418dc012f33b2c0c600ca18336d7d34
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-14 23:03:10 -0700

    Time: 2169 ms (27.02%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -1,32 +1,26 @@
-# Write your MySQL query statement belowse
+# Write your MySQL query statement below
 with cte as
-(select 
-t2.customer_id, 
-date_format(order_date,"%Y-%m") as "mon_val",
-t1.price*t2.quantity as "total"
-from product as t1 
-inner join
-orders as t2 
-on 
-t1.product_id = t2.product_id 
-),
+    (select customer_id, 
+        sum(price) as "total", 
+        date_value
+        from
+            (select 
+            t2.customer_id, 
+            t1.price*t2.quantity as "price", 
+            date_format(order_date,"%Y-%m") as "date_value"
+            from product as t1 inner join orders as t2 on 
+            t1.product_id = t2.product_id ) as t3 
+        group by customer_id, date_value),
+
 cte2 as
-(select customer_id, mon_val, sum(total)  as "total"
-from cte
-group by customer_id, mon_val),
+    (select distinct p1.customer_id from cte as p1, cte as p2
+    where p1.customer_id = p2.customer_id and 
+    p1.date_value = "2020-06" and p2.date_value = "2020-07" and 
+    p1.total >= 100 and p2.total >= 100)
 
-cte3 as
-(select p1.customer_id from cte2 as p1, cte2 as p2 
-where 
-p1.customer_id = p2.customer_id and 
-p1.mon_val = "2020-06" and 
-p2.mon_val = "2020-07" and
-p1.total >= 100 and 
-p2.total >= 100)
-
-select q1.customer_id, q2.name from 
-cte3 as q1 
+select 
+q2.customer_id, q2.name 
+from cte2 as q1 
 inner join 
-customers as q2 
-on 
-q1.customer_id = q2.customer_id 
+customers as q2 on 
+q1.customer_id = q2.customer_id ; 
