5e57bae32d129403e22043caa5a6f025446a355b
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-16 12:16:29 -0700

    Time: 1506 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,16 +1,11 @@
 # Write your MySQL query statement below
 with cte as
-    (select managerid
-        from
-        (select managerid, count(id) as "counting"
+    (select managerId from
+        (select managerId, count(*) as "total"
         from employee
-        group by managerid) as t1
-        where counting >= 5)
+        group by managerId ) as t1 
+        where total >= 5)
 
-select 
-p2.name 
-from cte as p1 
-inner join 
-employee as p2 
-on 
-p1.managerid = p2.id
+select p2.name from cte as p1 
+inner join employee as p2 
+on p1.managerid = p2.id 
