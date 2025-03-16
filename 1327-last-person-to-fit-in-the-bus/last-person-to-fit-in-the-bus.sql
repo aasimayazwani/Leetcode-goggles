ecf21420e496a9a398990ace958370a2f9225663
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-16 12:42:07 -0700

    Time: 1135 ms (86.26%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,9 +1,11 @@
+# Write your MySQL query statement below
+with cte as
+    (select person_name, Total_Weight from
+        (select person_name, sum(weight) over (order by turn asc) as "Total_Weight"
+        from queue) as t1 
+        where Total_Weight <= 1000)
+
 select person_name from
-    (select person_name, total_weight
-        from
-        (select *, sum(weight) over (order by turn) as "total_weight"
-        from queue ) as t1 
-        where total_weight <= 1000 ) as t2 
-    order by total_weight desc 
-    limit 1 ; 
-    
+    (select *, dense_rank() over (order by Total_Weight desc ) as "ranking" 
+    from cte) as t2 
+    where ranking = 1 
