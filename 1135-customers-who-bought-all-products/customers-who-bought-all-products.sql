a1f19be4d3917b896068c06fb59bd0fb25fe1c12
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-16 12:07:29 -0700

    Time: 1641 ms (9.46%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,6 +1,6 @@
-select customer_id from 
-(select customer_id, count(distinct product_key) as "total"
-from customer
-group by customer_id ) as t1 
-where 
-total = (select count(product_key) from product ) ; 
+# Write your MySQL query statement below
+select distinct customer_id from
+    (select customer_id, count(distinct product_key) as "counting"
+    from customer
+    group by customer_id) as t1 
+    where counting = (select count(*) from product)
