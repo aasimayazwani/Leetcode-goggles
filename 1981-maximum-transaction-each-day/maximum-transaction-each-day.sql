f08d4d6602584a9f8d7cc5d65be1ea9a2f4ff440
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-15 18:15:42 -0700

    Time: 1628 ms (10.68%) | Memory: 0B (100.00%) - LeetSync

--- 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
 select transaction_id
-from 
-(select *, dense_rank() over (partition by day order by amount desc) as "ranking"
-from
-    (select transaction_id,amount,date_format(day,"%Y-%m-%d") as "day"
-    from transactions) 
-    as t1 ) as t2 
-where ranking = 1
-order by transaction_id asc  ; 
+    from
+    (select *, 
+        dense_rank() over (partition by day order by amount desc) as "ranking"
+        from transactions) as t1 
+    where ranking = 1 
+    order by transaction_id asc ; 
