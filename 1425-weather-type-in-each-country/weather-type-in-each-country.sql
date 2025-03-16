20d337a2424b56f95a1efbd72611124779745375
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-15 20:07:01 -0700

    Time: 1180 ms (48.02%) | Memory: 0B (100.00%) - LeetSync

--- 1425-weather-type-in-each-country/weather-type-in-each-country.sql
+++ 1425-weather-type-in-each-country/weather-type-in-each-country.sql
@@ -1,27 +1,23 @@
 # Write your MySQL query statement below
 with cte as
-(select country_id, temp
-    from
-    (select country_id, 
-            avg(weather_state) as "temp",
-            date_format(day,"%Y-%m") as "val"
-            from weather
-            group by country_id, val) as t1 
-    where 
-    val = "2019-11"),
+    (select country_id, avg(weather_state) as "temp", value
+        from
+        (select country_id, weather_state, date_format(day,"%Y-%m") as "value"
+        from weather) as t1
+        group by country_id, value),
 
 cte2 as
-(select country_id, 
-        case 
-            when temp <= 15 then "Cold"
-            when temp >= 25 then "Hot"
-            else "Warm"
-        end as "weather_type"
-        from cte)
+    (select country_id, 
+    case 
+        when temp <= 15 then "Cold"
+        when temp >= 25 then "Hot" 
+        else "Warm"
+    end as "weather_type"
+    from cte 
+    where value = "2019-11")
 
-select p2.country_name, p1.weather_type from 
-    cte2 as p1 
-    inner join 
-    countries as p2 
-    on 
-    p1.country_id = p2.country_id ; 
+select 
+p2.country_name, p1.weather_type
+from cte2 as p1 
+inner join countries as p2 on 
+p1.country_id = p2.country_id
