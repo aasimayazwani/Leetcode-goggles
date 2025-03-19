30d2a75e46ffb446ef10e9bd999ee3ac2c533f17
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-18 18:28:01 -0700

    Time: 2270 ms (8.29%) | Memory: 0B (100.00%) - LeetSync

--- 1821-products-worth-over-invoices/products-worth-over-invoices.sql
+++ 1821-products-worth-over-invoices/products-worth-over-invoices.sql
@@ -1,18 +1,21 @@
 # Write your MySQL query statement below
 with cte as
-    (select product_id ,
-            sum(rest) as "rest", 
-            sum(paid) as "paid", 
-            sum(canceled) as "canceled",
-            sum(refunded) as  "refunded"
-    from invoice 
-    group by product_id)
+    (select product_id, sum(rest) as "rest", 
+                sum(paid) as "paid", 
+                sum(canceled) as "canceled", 
+                sum(refunded) as "refunded"
+                from invoice
+            group by product_id)
 
-select t2.name, ifnull(t1.rest,0) as "rest",
-                ifnull(t1.paid,0) as "paid",
-                ifnull(t1.canceled,0) as "canceled",
-                ifnull(t1.refunded,0) as "refunded" from product as t2 
+select  
+p2.name, 
+ifnull(p1.rest,0) as "rest", 
+ifnull(p1.paid,0) as "paid", 
+ifnull(p1.canceled,0) as "canceled", 
+ifnull(p1.refunded,0) as "refunded"
+from product as p2
 left join 
- cte as t1 
-on t2.product_id  =  t1.product_id
-order by t2.name
+cte as p1 
+on 
+p1.product_id = p2.product_id 
+order by p2.name asc ;
