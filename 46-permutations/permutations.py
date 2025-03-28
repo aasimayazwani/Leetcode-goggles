d5392ffb5accd7b09d8c0ecae67cb791d1ddd1b7
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-27 23:52:06 -0700

    Time: 3 ms (32.69%) | Memory: 17.8 MB (80.36%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,12 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def backtrack(current,nums):
-            if len(current)==len(nums):
-                ans.append(current)
-            elif len(current) < len(nums):
-                for i in range(0,len(nums)):
-                    if nums[i] not in current:
-                        backtrack(current+[nums[i]],nums)
-        backtrack([],nums)
-        return list(set([tuple(item) for item in ans]))
+        def recurse(arr,nums):
+            if len(arr) == len(nums):
+                ans.append(arr)
+                return 
+            for i in range(0,len(nums)):
+                if nums[i] not in arr:
+                    recurse(arr+[nums[i]],nums)
+                else:
+                    pass
+        recurse([],nums)
+        return ans 
+        
+            
