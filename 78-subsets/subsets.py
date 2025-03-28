b667410acacc6cde0c363c75cac697bf0cf9f693
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-28 06:10:55 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.27%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,13 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = [[]]
-        def iterate(nums,current):
-            if len(nums) == 0:
-                result.append(current)
-            else:
-                if len(current) > 0:
-                    result.append(current)
-                for i in range(0,len(nums)):
-                    iterate(nums[i+1:],current+[nums[i]])
-        iterate(nums,[])
-        return result
+        def recurse(arr,nums,n):
+            if len(arr) < n:
+                ans.append(arr)
+            elif len(arr) == n:
+                ans.append(arr)
+                return
+
+            for i in range(0,len(nums)):
+                if len(arr) > 0:
+                    if arr[-1] < nums[i]:
+                        recurse(arr+[nums[i]],nums,n)
+                else:
+                    recurse(arr+[nums[i]],nums,n)
+        ans = []
+        recurse([],nums,len(nums))
+        return ans 
+            
