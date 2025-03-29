59bf089504c27e8780b60150e4272f5a220c3dd7
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-03-28 22:39:20 -0700

    Time: 486 ms (5.02%) | Memory: 59.7 MB (57.46%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,18 +1,22 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        nums = list(range(1,n+1))
-        def search(cur,k):
-            if len(cur) == k:
-                ans.append(cur)
-                return 
-            else:
-                for i in range(1,n+1):
-                    if len(cur) ==0:
-                        search(cur+[i],k)
-                    else:
-                        if cur[-1] < i:
-                            search(cur+[i],k)
         ans = []
-        search([],k)
-        return ans 
+        initial = list(range(1,k+1))
+        def recurse(initial,current,n,k):
+            if len(current) == k:
+                ans.append(current)
+                return 
+            for i in range(1,n+1):
+                if len(current) > 0:
+                    if current[-1] < i:
+                        recurse(initial,current+[i],n,k)
+                else:
+                    recurse(initial,current+[i],n,k)
+
             
+        recurse(initial,[],n,k)
+        return ans
+
+
+            
+
