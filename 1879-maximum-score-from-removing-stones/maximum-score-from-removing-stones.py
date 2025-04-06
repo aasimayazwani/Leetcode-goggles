297607b215964167de1941a5b61d36b79b2b2378
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-04-05 20:54:46 -0700

    Time: 334 ms (12.09%) | Memory: 17.8 MB (53.85%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.py
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.py
@@ -1,18 +1,16 @@
 class Solution:
     def maximumScore(self, a: int, b: int, c: int) -> int:
-        min_stack = [-a,-b,-c]
-        heapify(min_stack)
-        score = 0
-        popped = []
-        while len(min_stack) > 1:
-            a = heappop(min_stack) + 1 
-            b = heappop(min_stack) + 1 
-            score +=1
-            if a < 0:
-                heappush(min_stack,a)
-            if b < 0:
-                heappush(min_stack,b)
-        return score 
-
-            
-            
+        arr = [a,b,c]
+        arr = [-item for item in arr]
+        heapq.heapify(arr)
+        # pick 2 non zero entries 
+        counting = 0
+        while len(arr) > 1:
+            x, y = heapq.heappop(arr), heapq.heappop(arr)
+            x, y = -1*(x), -1*(y)
+            counting +=1 
+            if x-1 > 0:
+                heapq.heappush(arr,-1*(x-1))
+            if y-1>0:
+                heapq.heappush(arr,-1*(y-1))
+        return counting 
